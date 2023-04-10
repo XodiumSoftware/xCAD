746e9d964b3a32314f1780a10bdf8e63d9c0258a
Illyrius <FitimQ@live.nl>
2023-04-10 15:59:05 +0200

    added all the icons

--- src/constants.py
+++ src/constants.py
@@ -14,17 +14,17 @@ MAIN_WINDOW_CONTENTS_MARGINS = (
 )
 MAIN_WINDOW_BUTTON_TEXTS = (
     'Draw TFC',
-    'Calculations',
+    'Calculator',
     'COMING SOON',
     'COMING SOON',
     'COMING SOON',
 )
 MAIN_WINDOW_ICON_PATHS = (
     "src/images/dtfc_icon_light.png",
-    "",
-    "",
-    "",
-    "",
+    "src/images/calc_icon_light.png",
+    "src/images/coming-soon_icon_light.png",
+    "src/images/coming-soon_icon_light.png",
+    "src/images/coming-soon_icon_light.png",
 )
 MAIN_WINDOW_BUTTON_SIZE = 50, 50
 WINDOW_STYLE = "fusion"
