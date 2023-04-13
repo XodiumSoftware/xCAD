77ddfe714277cfe40b57deff26c4846eab1a81a7
Illyrius <FitimQ@live.nl>
2023-04-13 16:18:28 +0200

    Organized the constants

--- src/constants.py
+++ src/constants.py
@@ -1,15 +1,18 @@
-# These are variables that store values related to the user interface of a Timber Frame Constructions
-# CAD software. They include the title of the main window, the title of a group box, the path to the
-# main window icon, the margins of the main window contents, the texts and size of buttons in the main
-# window, the window style, the path to the icons of the main window buttons, and the copyright label.
-# These variables can be used to customize the appearance and functionality of the software.
+# All UI
 UI_TITLE = "TFCCAD"
-MAIN_UI_GROUPBOX_TITLE = "Timber Frame Constructions CAD"
-TFCC_UI_GROUPBOX_TITLE = "Timber Frame Constructions Creator"
+UI_ICON_PATH = "src/icons/ui_icon.png"
 UI_GROUPBOX_FONT_TYPE = "Arial"
 UI_GROUPBOX_FONT_SIZE = 10
-UI_ICON_PATH = "src/icons/ui_icon.png"
 UI_CONTENTS_MARGINS = (1, 1, 1, 1)
+UI_STYLE = "fusion"
+COPYRIGHT_LABEL = "©Qerimi Engineering"
+COPYRIGHT_LABEL_SIZE = 8
+COPYRIGHT_LABEL_STYLE = "font-style: italic;"
+UI_GROUPBOX_STYLESHEET = "QGroupBox { border: 0; padding-top: 20; }"
+ICON_TO_BUTTON_MARGIN = 10
+
+# Main UI
+MAIN_UI_GROUPBOX_TITLE = "Timber Frame Constructions CAD"
 MAIN_UI_BUTTON_TEXTS = (
     "TFC Creator",
     "Calculator",
@@ -26,12 +29,9 @@ MAIN_UI_ICON_PATHS = (
 )
 MAIN_UI_BUTTON_HEIGHT = 50
 MAIN_UI_BUTTON_WIDTH = MAIN_UI_BUTTON_HEIGHT
-UI_STYLE = "fusion"
-COPYRIGHT_LABEL = "©Qerimi Engineering"
-COPYRIGHT_LABEL_SIZE = 8
-COPYRIGHT_LABEL_STYLE = "font-style: italic;"
-UI_GROUPBOX_STYLESHEET = "QGroupBox { border: 0; padding-top: 20; }"
-ICON_TO_BUTTON_MARGIN = 10
+
+# TFCC UI
+TFCC_UI_GROUPBOX_TITLE = "Timber Frame Constructions Creator"
 TFCC_UI_GROUPBOX_INPUT_FIELDS_DESC0 = (
     "Frame Length",
     "Frame Height",
@@ -51,6 +51,8 @@ TFCC_UI_GROUPBOX_INPUT_FIELDS_DESC1 = (
     "18",
 )
 TFCC_UI_GROUPBOX_INPUT_FIELDS_DESC2 = ("mm", "mm", "", "mm", "mm", "", "mm")
+
+# Other UI
 BACK_BUTTON = "Back"
 SAVE_BUTTON = "Save"
 SAVE_UI_TEXT = "Changes Saved"
