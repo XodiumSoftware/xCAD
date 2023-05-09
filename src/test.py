06622084b0799b35c4e1f711caefe00014f64d98
Illyrius <FitimQ@live.nl>
2023-05-09 13:57:08 +0200

    test v2

--- src/test.py
+++ src/test.py
@@ -1,18 +1,24 @@
-from BricscadApp import *
-from Teigha.DatabaseServices import *
-from Teigha.Geometry import *
+import sys
 
+from ezdxf import *
 
-@CommandMethod("MYCOMMAND")
-def my_command():
-    ed = Application.DocumentManager.MdiActiveDocument.Editor
-    p1 = ed.GetPoint("Select the first point:")
-    p2 = ed.GetPoint("Select the second point:", p1.Value)
-    with HostApplicationServices.WorkingDatabase.TransactionManager.StartTransaction() as tr:
-        line = Line(p1.Value, p2.Value)
-        tr.AddNewlyCreatedDBObject(line, True)
-        space = tr.GetObject(
-            DocumentManager.MdiActiveDocument.Database.CurrentSpaceId, OpenMode.ForWrite
-        )
-        space.AppendEntity(line)
-        tr.Commit()
+try:
+    # get the filepath of the existing DXF document from the command line argument
+    filepath = sys.argv[1]
+
+    # open the existing DXF document
+    doc = readfile(filepath)
+    msp = doc.modelspace()  # get the modelspace
+
+    # prompt the user to enter two points
+    p1 = tuple(map(float, input("Enter the first point (x, y): ").split()))
+    p2 = tuple(map(float, input("Enter the second point (x, y): ").split()))
+
+    # add a line to the modelspace using the selected points
+    line = msp.add_line(p1, p2)
+
+    # save the DXF document
+    doc.save()
+
+except Exception as e:
+    print("An error occurred:", e)
