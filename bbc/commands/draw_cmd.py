41c48820506a16191d92e63ced3e4a593b39e596
Illyrius <FitimQ@live.nl>
2023-05-12 15:46:13 +0200

    -

--- bbc/commands/draw_cmd.py
+++ bbc/commands/draw_cmd.py
@@ -8,7 +8,6 @@ def draw_line_between_two_points(start_point, end_point):
     """
     Adds a line to the drawing
     """
-    # TODO: Remove duplicated code from bbc/wrappers/bricscad_wrapper.py or the other way around depending whats best.
     # Create a new line entity
     bricscad_wrapper.add_line(start_point, end_point)
 
