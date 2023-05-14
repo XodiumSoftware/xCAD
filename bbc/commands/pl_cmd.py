c92ebcb302900bfda6afd275ea29c430dff4d7d9
Illyrius <FitimQ@live.nl>
2023-05-14 21:44:23 +0200

    -

--- bbc/commands/pl_cmd.py
+++ bbc/commands/pl_cmd.py
@@ -4,42 +4,40 @@ import comtypes.client
 from pyautocad import APoint, Autocad
 
 
-# Get the active AutoCAD application and document
-@lru_cache(maxsize=None)
-def get_autocad_app():
-    return comtypes.client.GetActiveObject("AutoCAD.Application")
+class AutoCAD:
+    def __init__(self):
+        self.acad = self.get_active_acad_app()
+        self.autocad = Autocad(create_if_not_exists=True)
+        self.doc = self.autocad.doc
+        self.model_space = self.doc.ModelSpace
+        self.handler = AutocadEventHandler(self.autocad, self.model_space)
 
+    @staticmethod
+    @lru_cache(maxsize=None)
+    def get_active_acad_app():
+        return comtypes.client.GetActiveObject("AutoCAD.Application")
 
-acad = get_autocad_app()
-doc = acad.DocumentManager.MdiActiveDocument
-model_space = doc.ModelSpace
+    def run(self):
+        self.doc.CommandEnded.add(self.handler.on_command_entered)
+        self.doc.SendCommand("_pline2\n")
 
 
 class AutocadEventHandler:
-    def __init__(self, acad):
-        self.acad = acad
+    def __init__(self, autocad, model_space):
+        self.autocad = autocad
+        self.model_space = model_space
 
-    # Define a function to create a polyline between two points
-    @staticmethod
-    def create_polyline(start_point, end_point):
-        return model_space.AddLine(start_point, end_point)
+    def create_polyline(self, start_point, end_point):
+        return self.model_space.AddLine(start_point, end_point)
 
-    # Define a function to handle the command line input
     def on_command_entered(self, sender, args):
-        # Check if the entered command is "pline2"
         if args.commandName.lower() == "pline2":
-            # Prompt the user to enter the start and end points
-            start_point, end_point = APoint(self.acad.GetPoint()), APoint(
-                self.acad.GetPoint()
+            start_point, end_point = APoint(self.autocad.GetPoint()), APoint(
+                self.autocad.GetPoint()
             )
-            # Create the polyline between the two points
             self.create_polyline(start_point, end_point)
 
 
-# Use the with_events decorator to register the command entered event handler
-handler = AutocadEventHandler(acad)
-acad = Autocad(create_if_not_exists=True)
-acad.doc.CommandEnded.add(handler.on_command_entered)
-
-# Run the AutoCAD event loop
-acad.doc.SendCommand("_pline2\n")
+if __name__ == "__main__":
+    autocad = AutoCAD()
+    autocad.run()
