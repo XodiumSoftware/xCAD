2eae857af12bd352fdf4fe38d5ddb1d4a57d5854
Illyrius <FitimQ@live.nl>
2023-05-15 15:48:59 +0200

    removed autocad bullshit

--- bbc/commands/test2.py
+++
@@ -1,8 +0,0 @@
-# importing pyautocad
-import pyautocad
-
-# creating AutoCAD instance
-acad = pyautocad.Autocad()
-
-# print name of document currently recognized as being active in AutoCAD
-print(acad.doc.Name)
