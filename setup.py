b08f44aa221f0bc312482cd93be99b412fe15dc3
Illyrius <FitimQ@live.nl>
2023-05-15 15:53:12 +0200

    merged some code since it was similar

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
 from Cython.Build import cythonize
 from setuptools import setup
 
-setup(
-    ext_modules=cythonize("pline_cmd.py"),
-)
-
 if __name__ == "__main__":
-    setup()
+    setup(
+        ext_modules=cythonize("pline_cmd.py"),
+    )
