44d271ad04c24d4436f2eac2e7597b872df1d890
Illyrius <FitimQ@live.nl>
2023-05-15 17:11:29 +0200

    -

--- bbc/commands/pline_cmd.pyx
+++ bbc/commands/pline_cmd.pyx
@@ -1,1 +1,26 @@
-print("Hello World")
+
+# TODO: replace "your_header_file.h" and "your_namespace" with the appropriate names.
+cdef extern from "your_header_file.h" namespace "your_namespace":
+    cdef cppclass AcGiGeometry:
+        virtual Adesk::Boolean polyline(const AcGiPolyline& giPolyline) const
+
+    cdef cppclass AcGiPolyline:
+        # Define the members of the AcGiPolyline class if necessary
+        # Add function here
+        pass
+
+
+cdef PyAcGiGeometry geometry = PyAcGiGeometry()
+
+# Create an instance of AcGiPolyline
+cdef AcGiPolyline giPolyline = AcGiPolyline()
+# Set up the giPolyline object if needed
+
+# Call the polyline function
+cdef Adesk::Boolean success = geometry.polyline(giPolyline)
+
+# Check the result
+if success == Adesk::kFalse:
+    print("Drawing the AcGiPolyline object was successful.")
+else:
+    print("Failed to draw the AcGiPolyline object.")
