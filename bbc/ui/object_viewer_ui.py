b09f0d9b08dee9d5335608dc020ea15fa0d56ea0
Illyrius <FitimQ@live.nl>
2023-05-16 23:03:32 +0200

    cleaned and renamed some stuff

---
+++ bbc/ui/object_viewer_ui.py
@@ -0,0 +1,12 @@
+from constants import *
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+# FIXME: UI size being too small when toggling config ui.
+
+
+# TODO: Rewrite CObjectViewerUI.
+class ObjectViewerUI:
+    def __init__(self):
+        pass
---
+++ bbc/ui/ui_setup.py
@@ -0,0 +1,1 @@
+# TODO: Add repeating code here.
