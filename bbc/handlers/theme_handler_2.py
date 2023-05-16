3758cd233a6bc3b76aed13101edad1e5cd99d22f
Illyrius <FitimQ@live.nl>
2023-05-16 17:42:39 +0200

    added detect system theme function

--- bbc/handlers/theme_handler_2.py
+++ bbc/handlers/theme_handler_2.py
@@ -1,3 +1,5 @@
+import winreg
+
 from PySide6.QtCore import QFile, QSettings, QTextStream
 from PySide6.QtWidgets import QApplication
 
@@ -10,6 +12,14 @@ SETTINGS_APPLICATION = "CurrentThemeSetting"
 KEY_THEME = "theme"
 KEY_THEME_LIGHT = "light"
 KEY_THEME_DARK = "dark"
+
+MS_VALUE_NAME = "AppsUseLightTheme"
+
+WINREG_THEME_KEY = "Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize"
+
+DEBUG_NAME = "[DEBUG] "
+
+DEBUG_ERROR_DETECTING_SYSTEM_THEME = DEBUG_NAME + "Error detecting system theme: "
 
 
 class ThemeHandler:
@@ -31,12 +41,37 @@ class ThemeHandler:
                 file.close()
         return stylesheet
 
+    def detect_system_theme(self):
+        try:
+            # Open the registry key that stores the system-wide color settings
+            key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, WINREG_THEME_KEY)
+
+            # Read the value of the "AppsUseLightTheme" key
+            # If the value is set to 1, the system is using the light theme
+            # If the value is set to 0, the system is using the dark theme
+            value_name = MS_VALUE_NAME
+            value = winreg.QueryValueEx(key, value_name)[0]
+
+            # Set the theme based on the system-wide color setting
+            if value == 0:
+                self.set_theme(KEY_THEME_DARK)
+            else:
+                self.set_theme(KEY_THEME_LIGHT)
+
+        except Exception as e:
+            print(DEBUG_ERROR_DETECTING_SYSTEM_THEME, e)
+
     def load_theme(self):
+        # Load the saved theme from the settings
         theme = self.settings.value(KEY_THEME, KEY_THEME_LIGHT)
+
         if theme == KEY_THEME_DARK:
             self.set_theme(KEY_THEME_DARK)
-        else:
+        elif theme == KEY_THEME_LIGHT:
             self.set_theme(KEY_THEME_LIGHT)
+        else:
+            # If no theme is saved, detect the system-wide color setting
+            self.detect_system_theme()
 
     def set_theme(self, theme):
         if theme == KEY_THEME_DARK:
