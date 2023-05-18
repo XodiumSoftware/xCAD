639d8986bad0ff46f140f68cd9efcdaee8d95591
Illyrius <FitimQ@live.nl>
2023-05-18 17:17:21 +0200

    -

--- bbc/ui/main_ui.py
+++ bbc/ui/main_ui.py
@@ -14,11 +14,9 @@ class MainUI(QMainWindow, ObjectViewerUI, UIHandler, ThemeHandler, EventsHandler
         super().__init__()
 
         # Call functions here.
-        self.setCentralWidget(QWidget())
+        self.setCentralWidget(QWidget())  # Set the central widget
         self.initMainUI()
         self.quit_on_key_press_event()
-
-        # self.set_theme_handler(KEY_THEME_LIGHT) # FIXME: AttributeError
 
     def initMainUI(self):
         # Set up the main window
@@ -28,25 +26,28 @@ class MainUI(QMainWindow, ObjectViewerUI, UIHandler, ThemeHandler, EventsHandler
         settings_widget = SettingsListWidget(self)
         total_columns_width = settings_widget.get_total_columns_width()
 
+        # Create a new layout for the central widget
+        central_layout = QGridLayout()
+        self.centralWidget().setLayout(central_layout)
+
+        # Add Modules to the layout
+        central_layout.addWidget(ButtonWidget(self), 0, 0)
+        central_layout.addWidget(settings_widget, 1, 0)
+
         # Calculate the desired width of the main window
-        total_columns_width += self.centralWidget().layout().spacing() * (
+        total_columns_width += central_layout.spacing() * (
             settings_widget.table_widget.columnCount() - 1
         )
         window_width = (
             total_columns_width
-            + self.centralWidget().layout().contentsMargins().left()
-            + self.centralWidget().layout().contentsMargins().right()
+            + central_layout.contentsMargins().left()
+            + central_layout.contentsMargins().right()
         )
 
         # Set the width of the main window
         self.setFixedWidth(window_width)
 
-        # Create a new layout for the central widget
-        layout = QGridLayout(self.centralWidget())
-
-        # Add Modules to the layout
-        layout.addWidget(ButtonWidget(self), 0, 0)
-        layout.addWidget(settings_widget, 1, 0)
-
         # Center the window on the primary screen
         self.center_ui_on_screen_handler(self)
+
+        # TODO: Make the width of the app still resizable by the user.
