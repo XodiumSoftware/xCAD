56ce89f0850ece522f83a5f7cb9bdbbf55330f59
Illyrius <FitimQ@live.nl>
2023-05-28 22:47:26 +0200

    -

--- bbc/ui/modules/settings_list_widget.py
+++ bbc/ui/modules/settings_list_widget.py
@@ -9,16 +9,52 @@ from PySide6.QtWidgets import (
     QWidget,
 )
 
+# TODO: Move the constants to "constants.py".
+# TODO: Make the cells have different setting_types.
+# TODO: define the setting_types.
+# TODO: group_name be bold.
+# TODO: group_name be centered.
+# TODO: group header have background color.
+# TODO: column header have background color.
+# TODO: group be foldable to hide/show the items within.
+# TODO: Make the content be saved in the SQLite database based on the save/discard button.
+# TODO: get the if saved data from the database and display it.
+# TODO: change the DATA items content to the appropriate content and dont forget to remove the 3rd column.
+# TODO: rename the column to an appropriate name based on the items.
+
 DATA = [
-    ("John Doe", 30, "USA"),
-    ("Jane Smith", 25, "Canada"),
-    ("Bob Johnson", 45, "UK"),
-    ("Alice Brown", 35, "Australia"),
+    {
+        "group_name": "Group 1",
+        "items": [
+            ("John Doe", 30, "USA"),
+            ("Jane Smith", 25, "Canada"),
+            ("Bob Johnson", 45, "UK"),
+            ("Alice Brown", 35, "Australia"),
+        ],
+    },
+    {
+        "group_name": "Group 2",
+        "items": [
+            ("Michael Williams", 50, "Germany"),
+            ("Emma Johnson", 28, "France"),
+            ("William Davis", 42, "Spain"),
+            ("Olivia Taylor", 32, "Italy"),
+        ],
+    },
+    {
+        "group_name": "Group 3",
+        "items": [
+            ("James Wilson", 37, "Brazil"),
+            ("Sophia Martinez", 31, "Mexico"),
+            ("Daniel Anderson", 48, "Argentina"),
+            ("Mia Thomas", 29, "Chile"),
+        ],
+    },
 ]
 
 HEADER_LABELS = ["Name", "Age", "Country"]
 HEADER_STYLE = ["Arial", 10, QFont.Weight.Bold]
-CELL_STYLE = ["Arial", 8, None]
+CELL_STYLE = ["Arial", 8]
 
 
 class SettingsListWidget(QWidget):
@@ -40,35 +76,59 @@ class SettingsListWidget(QWidget):
         layout.addWidget(self.table_widget)
 
         self.populate_table()
-        self.style_cells()
         self.style_header()
         self.setup_header_sorting()
+        self.set_cell_size()
 
     def populate_table(self):
         """
-        Populate the table.
+        Populate the table with data.
         """
-        for row, (name, age, country) in enumerate(DATA):
-            self.table_widget.insertRow(row)
-            self.table_widget.setItem(row, 0, QTableWidgetItem(name))
-            self.table_widget.setItem(row, 1, QTableWidgetItem(str(age)))
-            self.table_widget.setItem(row, 2, QTableWidgetItem(country))
+        for group_data in DATA:
+            group_name = group_data["group_name"]
+            group_items = group_data["items"]
 
-            for column in range(1, self.table_widget.columnCount()):
-                item = self.table_widget.item(row, column)
-                item.setTextAlignment(Qt.AlignmentFlag.AlignCenter)
+            group_item = QTableWidgetItem(
+                group_name
+            )  # FIXME: This making the items be a NoneType which is a problem since i wont be able to assign properties to it.
+            group_item.setFlags(Qt.ItemFlag.NoItemFlags)
+            self.table_widget.insertRow(self.table_widget.rowCount())
+            self.table_widget.setItem(self.table_widget.rowCount() - 1, 0, group_item)
 
-            self.table_widget.setRowHeight(row, 20)
+            for item in group_items:
+                name, age, country = item
+                self.table_widget.insertRow(self.table_widget.rowCount())
+
+                name_item = QTableWidgetItem(name)
+                self.table_widget.setItem(
+                    self.table_widget.rowCount() - 1, 0, name_item
+                )
+
+                age_item = QTableWidgetItem(str(age))
+                self.table_widget.setItem(self.table_widget.rowCount() - 1, 1, age_item)
+
+                country_item = QTableWidgetItem(country)
+                self.table_widget.setItem(
+                    self.table_widget.rowCount() - 1, 2, country_item
+                )
+
+        self.style_cells()
 
     def style_cells(self):
         """
         Style the cells.
         """
-        cell_font = QFont(CELL_STYLE)
+        cell_font = QFont(*CELL_STYLE)
         for row in range(self.table_widget.rowCount()):
             for column in range(self.table_widget.columnCount()):
                 item = self.table_widget.item(row, column)
-                item.setFont(cell_font)
+                item.setFont(
+                    cell_font
+                )  # FIXME: AttributeError: 'NoneType' object has no attribute 'setFont'
+                if column >= 2:
+                    item.setTextAlignment(
+                        Qt.AlignmentFlag.AlignHCenter
+                    )  # FIXME: AttributeError: 'NoneType' object has no attribute 'setTextAlignment'
 
     def style_header(self):
         """
@@ -81,5 +141,20 @@ class SettingsListWidget(QWidget):
         header.setFont(header_font)
 
     def setup_header_sorting(self):
+        """
+        Setup the header sorting.
+        """
         self.table_widget.setSortingEnabled(True)
         self.table_widget.sortItems(0, Qt.SortOrder.AscendingOrder)
+
+    def set_cell_size(self):
+        """
+        Set the cell size.
+        """
+        table_width = self.table_widget.viewport().width()
+        column_count = self.table_widget.columnCount()
+
+        column_width = table_width // column_count
+
+        for column in range(column_count):
+            self.table_widget.setColumnWidth(column, column_width)
