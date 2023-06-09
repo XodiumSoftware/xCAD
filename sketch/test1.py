b6b547b774ebcbc712efbe04278c4b1aadbb514f
Illyrius <FitimQ@live.nl>
2023-06-09 13:06:04 +0200

    -

--- sketch/test1.py
+++ sketch/test1.py
@@ -76,35 +76,35 @@ class MyWidget(QWidget):
 
         self.stacked_widget = QStackedWidget()
 
-        containers = [
+        self.containers = [
             ("Container 0", QPushButton("Toggle Page")),
             ("Container 1", QPushButton("Toggle Page")),
             ("Container 2", QPushButton("Toggle Page")),
         ]
 
-        for container_title, container_button in containers:
+        self.container_buttons = {}
+        for container_title, container_button in self.containers:
             container_widget = QWidget()
             container_layout = QVBoxLayout(container_widget)
             container_label = QLabel(container_title)
             container_button.clicked.connect(
-                lambda _, index=containers.index(
-                    (container_title, container_button)
-                ): self.toggleContainerVisibility(index)
+                lambda checked, button=container_button: self.toggleContainerVisibility(
+                    button
+                )
             )
             container_layout.addWidget(container_label)
             container_layout.addWidget(container_button)
             self.stacked_widget.addWidget(container_widget)
+
+            self.container_buttons[container_button] = len(self.container_buttons)
 
         self.stacked_widget.setCurrentIndex(int(layout_visible))
 
         main_layout = QVBoxLayout(self)
         main_layout.addWidget(self.stacked_widget)
 
-    def toggleContainerVisibility(self, container_index):
-        current_index = self.stacked_widget.currentIndex()
-        if current_index == container_index:
-            return
-
+    def toggleContainerVisibility(self, button):
+        container_index = self.container_buttons[button]
         self.stacked_widget.setCurrentIndex(container_index)
         print(container_index)
         self.settings_manager.set_setting("layout_visibility", str(container_index))
