c36b6d020c1839d5c76b5ddf16b8d29f47f813fa
Illyrius <FitimQ@live.nl>
2023-06-09 16:13:13 +0200

    -

--- sketch/example.pyx
+++ sketch/example.pyx
@@ -1,24 +1,26 @@
-# Import the required Cython modules
-from libc.stdlib cimport malloc, free
-
-# Define the factorial function in Cython
 cdef long long factorial_cython(int n):
+    """
+    Calculate the factorial of a given integer using Cython.
+    """
     cdef long long result = 1
     cdef int i
     for i in range(1, n + 1):
         result *= i
     return result
 
-# Define a Python wrapper function for the factorial function
 def factorial(n):
+    """
+    Calculate the factorial of a given integer.
+    """
     return factorial_cython(n)
 
-# Define a main function to demonstrate the usage
 def main():
+    """
+    Demonstrate the usage of the factorial function.
+    """
     cdef int n = 5
-    result = factorial(n)
+    cdef long long result = factorial(n)
     print(f"The factorial of {n} is: {result}")
 
-# Call the main function
 if __name__ == '__main__':
     main()
