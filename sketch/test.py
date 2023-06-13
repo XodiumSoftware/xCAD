87dabd8a72b48fc65ef0b21de8e437f4cb205b2f
Illyrius <FitimQ@live.nl>
2023-06-13 18:54:05 +0200

    -

---
+++ sketch/test.py
@@ -0,0 +1,9 @@
+# Call QMainWindow
+
+# Create 2 containers from QWidget
+
+# Add layout to each container
+
+# Add childwidgets to the respective layouts
+
+# create a hide/show function for the container
