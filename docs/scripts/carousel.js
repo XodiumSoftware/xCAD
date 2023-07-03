d6bde742815fb4d74e5d90a2820b3f7b2d4d2df2
Illyrius <FitimQ@live.nl>
2023-07-03 14:12:01 +0200

    -

--- docs/scripts/carousel.js
+++ docs/scripts/carousel.js
@@ -3,12 +3,12 @@ const buttons = document.querySelectorAll("[data-carousel-button]")
 buttons.forEach(button => {
   button.addEventListener("click", () => {
     const offset = button.dataset.carouselButton === "next" ? 1 : -1
-    const slides = button.closest("[data-carousel]").querySelector("[data-slide]")
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
 
     const activeSlide = slides.querySelector("[data-active]")
-
     let newIndex = [...slides.children].indexOf(activeSlide) + offset
-
     if (newIndex < 0) newIndex = slides.children.length - 1
     if (newIndex >= slides.children.length) newIndex = 0
 
