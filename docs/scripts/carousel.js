9b1e9818ce55960d32b3b8d9ff4b592cc60c9272
Illyrius <FitimQ@live.nl>
2023-07-04 11:31:31 +0200

    fixed the carousel bugging out on scroll. encapsulated the carousel script into the carousel class.

--- docs/scripts/carousel.js
+++ docs/scripts/carousel.js
@@ -1,57 +1,71 @@
-const buttons = document.querySelectorAll("[data-carousel-button]");
-const carousel = document.querySelector("[data-carousel]");
-const slides = carousel.querySelector("[data-slides]");
+class Carousel {
+  constructor(carouselElement) {
+    this.carousel = carouselElement;
+    this.slides = this.carousel.querySelector("[data-slides]");
+    this.buttons = this.carousel.querySelectorAll("[data-carousel-button]");
+    this.currentIndex = 0;
+    this.intervalId = null;
+    this.intervalTime = 3000;
 
-let currentIndex = 0;
-const intervalTime = 3000;
-let intervalId;
+    this.changeSlide = this.changeSlide.bind(this);
+    this.startSlideInterval = this.startSlideInterval.bind(this);
+    this.pauseSlideInterval = this.pauseSlideInterval.bind(this);
+    this.isCarouselVisible = this.isCarouselVisible.bind(this);
 
-function changeSlide(offset) {
-  const activeSlide = slides.querySelector("[data-active]");
-  activeSlide.removeAttribute("data-active");
+    this.setupCarousel();
+  }
 
-  currentIndex += offset;
-  if (currentIndex < 0) currentIndex = slides.children.length - 1;
-  if (currentIndex >= slides.children.length) currentIndex = 0;
+  changeSlide(offset) {
+    const activeSlide = this.slides.querySelector("[data-active]");
+    activeSlide.removeAttribute("data-active");
 
-  slides.children[currentIndex].setAttribute("data-active", true);
+    this.currentIndex += offset;
+    if (this.currentIndex < 0) this.currentIndex = this.slides.children.length - 1;
+    if (this.currentIndex >= this.slides.children.length) this.currentIndex = 0;
+
+    this.slides.children[this.currentIndex].setAttribute("data-active", true);
+  }
+
+  startSlideInterval() {
+    this.intervalId = setInterval(() => {
+      if (this.isCarouselVisible()) {
+        this.changeSlide(1);
+      }
+    }, this.intervalTime);
+  }
+
+  pauseSlideInterval() {
+    clearInterval(this.intervalId);
+  }
+
+  isCarouselVisible() {
+    const rect = this.carousel.getBoundingClientRect();
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+    const carouselHeight = rect.height;
+
+    return (
+      rect.top >= -carouselHeight * 0.5 &&
+      rect.bottom <= windowHeight + carouselHeight * 0.5
+    );
+  }
+
+  setupCarousel() {
+    this.buttons.forEach(button => {
+      button.addEventListener("click", () => {
+        const offset = button.dataset.carouselButton === "next" ? 1 : -1;
+        this.changeSlide(offset);
+      });
+
+      button.addEventListener("mouseenter", this.pauseSlideInterval);
+      button.addEventListener("mouseleave", this.startSlideInterval);
+    });
+
+    this.slides.addEventListener("mouseenter", this.pauseSlideInterval);
+    this.slides.addEventListener("mouseleave", this.startSlideInterval);
+
+    this.startSlideInterval();
+  }
 }
 
-function startSlideInterval() {
-  intervalId = setInterval(() => {
-    if (isCarouselVisible()) {
-      changeSlide(1);
-    }
-  }, intervalTime);
-}
-
-function pauseSlideInterval() {
-  clearInterval(intervalId);
-}
-
-function isCarouselVisible() {
-  const rect = carousel.getBoundingClientRect();
-  const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-  const carouselHeight = rect.height;
-
-  return (
-    rect.top >= -carouselHeight * 0.5 &&
-    rect.bottom <= windowHeight + carouselHeight * 0.5
-  );
-}
-
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-    changeSlide(offset);
-  });
-
-  button.addEventListener("mouseenter", pauseSlideInterval);
-  button.addEventListener("mouseleave", startSlideInterval);
-});
-
-carousel.addEventListener("mouseenter", pauseSlideInterval);
-carousel.addEventListener("mouseleave", startSlideInterval);
-window.addEventListener("scroll", startSlideInterval);
-
-startSlideInterval();
+const carouselElement = document.querySelector("[data-carousel]");
+const carousel = new Carousel(carouselElement);
