3834640c09072e65e5ae3b98d8cb8422c65bbee6
Illyrius <FitimQ@live.nl>
2023-07-04 09:16:09 +0200

    -

--- docs/scripts/hotlink_protection.js
+++ docs/scripts/hotlink_protection.js
@@ -1,6 +1,6 @@
 (function () {
     // Array of authorized domains
-    var authorizedDomains = ['illyrius.me/AutoFrameCAD'];
+    var authorizedDomains = ['https://illyrius.me/AutoFrameCAD', 'https://github.com/illyrius666', 'https://github.com/illyrius666/AutoFrameCAD'];
 
     // Get the referring URL
     var referringURL = document.referrer;
