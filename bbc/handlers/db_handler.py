3201bcacb7ac8cdfcbcd54711aa0f45b85c40d8c
Illyrius <FitimQ@live.nl>
2023-07-05 09:46:15 +0200

    added validate_and_sanitize_identifier func

--- bbc/handlers/db_handler.py
+++ bbc/handlers/db_handler.py
@@ -15,7 +15,7 @@ class DataBaseHandler:
         """
         Setup the database.
         """
-        database_directory = os.path.dirname(os.path.abspath(self.DATABASE_PATH))
+        database_directory = os.path.dirname(os.path.abspath(DATABASE_PATH))
         if not os.path.exists(database_directory):
             os.makedirs(database_directory)
         self.setup_database_model()
@@ -24,27 +24,29 @@ class DataBaseHandler:
         """
         Setup the database model.
         """
-        conn = sqlite3.connect(self.DATABASE_PATH)
+        conn = sqlite3.connect(DATABASE_PATH)
         cursor = conn.cursor()
 
         for table_data in TABLES:
-            table_name = table_data["desc"]
+            table_name_identified = self.validate_and_sanitize_identifier(
+                table_data["desc"]
+            )
             columns = table_data["columns"]
             rows = table_data["rows"]
 
-            if not self.table_exists(cursor, table_name):
-                column_names = ["Id"] + columns
-                create_table_query = "CREATE TABLE {table_name} ({columns})".format(
-                    table_name=table_name, columns=", ".join(column_names)
+            column_names = ["Id"] + columns
+            column_names = [
+                self.validate_and_sanitize_identifier(column_name)
+                for column_name in column_names
+            ]
+
+            if not self.table_exists(cursor, table_name_identified):
+                create_table_query = (
+                    f"CREATE TABLE {table_name_identified} ({', '.join(column_names)})"
                 )
                 cursor.execute(create_table_query)
 
-                insert_query = (
-                    "INSERT INTO {table_name} VALUES ({placeholders})".format(
-                        table_name=table_name,
-                        placeholders=",".join(["?"] * len(column_names)),
-                    )
-                )
+                insert_query = f"INSERT INTO {table_name_identified} VALUES ({', '.join(['?'] * len(column_names))})"
                 values = [(i,) + tuple(row) for i, row in enumerate(rows)]
                 cursor.executemany(insert_query, values)
 
@@ -66,12 +68,18 @@ class DataBaseHandler:
         """
         Get all the data from a table.
         """
-        conn = sqlite3.connect(self.DATABASE_PATH)
+        conn = sqlite3.connect(DATABASE_PATH)
         cursor = conn.cursor()
-        select_query = "SELECT * FROM {}".format(table_name)
+
+        table_name_identified = self.validate_and_sanitize_identifier(table_name)
+
+        select_query = f"SELECT * FROM {table_name_identified}"
         cursor.execute(select_query)
+
         table_data = cursor.fetchall()
+
         conn.close()
+
         return table_data
 
     def save_table_data(self, table_name, table_data):
@@ -81,12 +89,14 @@ class DataBaseHandler:
         conn = sqlite3.connect(DATABASE_PATH)
         cursor = conn.cursor()
 
-        delete_query = "DELETE FROM {}".format(table_name)
+        table_name_identified = self.validate_and_sanitize_identifier(table_name)
+
+        delete_query = f"DELETE FROM {table_name_identified}"
         cursor.execute(delete_query)
 
-        insert_query = "INSERT INTO {table_name} VALUES ({placeholders})".format(
-            table_name=table_name, placeholders=",".join(["?"] * len(table_data[0]))
-        )
+        num_columns = len(table_data[0])
+        placeholders = ", ".join("?" * num_columns)
+        insert_query = f"INSERT INTO {table_name_identified} VALUES ({placeholders})"
         cursor.executemany(insert_query, table_data)
 
         conn.commit()
@@ -96,10 +106,14 @@ class DataBaseHandler:
         """
         Discard the data from a table.
         """
-        conn = sqlite3.connect(self.DATABASE_PATH)
+        conn = sqlite3.connect(DATABASE_PATH)
         cursor = conn.cursor()
-        delete_query = "DELETE FROM {}".format(table_name)
+
+        table_name_identified = self.validate_and_sanitize_identifier(table_name)
+
+        delete_query = f"DELETE FROM {table_name_identified}"
         cursor.execute(delete_query)
+
         conn.commit()
         conn.close()
 
@@ -107,9 +121,18 @@ class DataBaseHandler:
         """
         Reset the data from a table.
         """
-        conn = sqlite3.connect(self.DATABASE_PATH)
+        conn = sqlite3.connect(DATABASE_PATH)
         cursor = conn.cursor()
-        delete_query = "DELETE FROM {}".format(table_name)
+
+        table_name_identified = self.validate_and_sanitize_identifier(table_name)
+
+        delete_query = f"DELETE FROM {table_name_identified}"
         cursor.execute(delete_query)
+
         conn.commit()
         conn.close()
+
+    def validate_and_sanitize_identifier(self, identifier):
+        if not identifier.isidentifier():
+            raise ValueError(f"Invalid identifier: {identifier}")
+        return identifier.replace('"', '""')
