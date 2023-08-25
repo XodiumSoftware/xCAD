1d41ae886b3f83976f188868314c1fa65f366cab
Illyrius <FitimQ@live.nl>
2023-08-25 11:48:55 +0200

    object_dialog_editor moved to, object_delegate. moved also some properties. kept just the frame calc in the scene_delegete, fixed bug where studs would overlap. fixed bug where when changing stud width value, would change the frameY value now it keeps its original length unchanged.

--- bbc/delegates/graphics_object_delegate.py
+++ bbc/delegates/graphics_object_delegate.py
@@ -1,8 +1,18 @@
+import sys
+
+from constants import OBJECT_EDITOR_DIALOG_TITLE, UI_ICON_PATH
 from enums.afc_enums import StudSettings
 from enums.q_enums import BrushStyleTypes, GraphicsItemFlagTypes, PenStyleTypes
 from handlers.db_handler import DataBaseHandler
-from PySide6.QtGui import QBrush, QColor, QPen
-from PySide6.QtWidgets import QGraphicsRectItem
+from PySide6.QtGui import QBrush, QColor, QIcon, QPen
+from PySide6.QtWidgets import (
+    QDialog,
+    QDoubleSpinBox,
+    QGraphicsRectItem,
+    QGridLayout,
+    QLabel,
+    QPushButton,
+)
 
 Graphics_objects_table = (
     "graphics_objects",
@@ -21,26 +31,36 @@ Graphics_objects_table = (
 class GraphicsObjectDelegate(QGraphicsRectItem):
     """A class to represent a graphics object delegate."""
 
-    def __init__(self) -> None:
+    def __init__(self, x: int, y: int, w: int, h: int, r: int) -> None:
         """Initialize the graphics object delegate."""
         super().__init__()
-        self.setup_graphics_object()
+        self.setup_graphics_object(self, x, y, w, h, r)
 
-    def setup_graphics_object(self) -> None:
+    @staticmethod
+    def setup_graphics_object(
+        obj: QGraphicsRectItem, x: int, y: int, w: int, h: int, r: int
+    ) -> None:
         """Setup the graphics object delegate."""
         db_handler = DataBaseHandler()
-        self.setup_table(db_handler)
+        GraphicsObjectDelegate.setup_table(db_handler)
         db_props = db_handler.retrieve_data(*Graphics_objects_table)
 
-        self.setFlags(
+        obj.setFlags(
             GraphicsItemFlagTypes.ItemIsSelectable.value
             | GraphicsItemFlagTypes.ItemSendsGeometryChanges.value
         )
 
         if db_props:
-            self.saved_data(self, db_props)
+            GraphicsObjectDelegate.saved_data(obj, db_props)
         else:
-            self.init_data(self)
+            GraphicsObjectDelegate.init_data(obj)
+
+        obj.setPos(x, y)
+        obj.setRect(obj.rect().x(), obj.rect().y(), w, h)
+        obj.setRotation(r)
+        obj.mouseDoubleClickEvent = (
+            lambda event, obj=obj: GraphicsObjectDelegate.object_editor_dialog(obj)
+        )
 
     @staticmethod
     def setup_table(db_handler: DataBaseHandler) -> None:
@@ -82,3 +102,49 @@ class GraphicsObjectDelegate(QGraphicsRectItem):
         obj.setToolTip(StudSettings.Name.value)
 
         # TODO: save after initial setup
+
+    @staticmethod
+    def object_editor_dialog(obj: QGraphicsRectItem) -> None:
+        """Open a dialog when a object is pressed."""
+        dialog = QDialog()
+        dialog.setWindowTitle(OBJECT_EDITOR_DIALOG_TITLE)
+        dialog.setWindowIcon(QIcon(UI_ICON_PATH))
+
+        layout = QGridLayout()
+        dialog.setLayout(layout)
+
+        label = QLabel("Stud width:")
+
+        obj_size_spinbox = QDoubleSpinBox()
+        obj_size_spinbox.setRange(0, sys.float_info.max)
+        obj_size_spinbox.setSingleStep(1)
+        obj_size_spinbox.setDecimals(0)
+        obj_size_spinbox.setValue(obj.rect().width())
+
+        save_button = QPushButton("Save")
+        discard_button = QPushButton("Discard")
+
+        save_button.clicked.connect(dialog.accept)
+        discard_button.clicked.connect(dialog.reject)
+
+        layout.addWidget(label, 0, 0)
+        layout.addWidget(obj_size_spinbox, 0, 1)
+        layout.addWidget(save_button, 1, 0)
+        layout.addWidget(discard_button, 1, 1)
+
+        dialog.setLayout(layout)
+        dialog.setMinimumSize(dialog.sizeHint())
+        dialog.setMaximumSize(dialog.sizeHint())
+
+        result = dialog.exec()
+
+        if result == QDialog.DialogCode.Accepted:
+            obj.setRect(
+                obj.rect().x(),
+                obj.rect().y(),
+                obj_size_spinbox.value(),
+                obj.rect().height(),
+            )
+            print("Accepted")
+        elif result == QDialog.DialogCode.Rejected:
+            print("Rejected")
--- bbc/delegates/graphics_scene_delegate.py
+++ bbc/delegates/graphics_scene_delegate.py
@@ -1,18 +1,8 @@
-import sys
 from typing import Dict
 
-from constants import OBJECT_EDITOR_DIALOG_TITLE, UI_ICON_PATH
 from delegates.graphics_object_delegate import GraphicsObjectDelegate
 from enums.afc_enums import FrameSettings, StudSettings
-from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import (
-    QDialog,
-    QDoubleSpinBox,
-    QGraphicsScene,
-    QGridLayout,
-    QLabel,
-    QPushButton,
-)
+from PySide6.QtWidgets import QGraphicsScene
 
 
 class GraphicsSceneDelegate(QGraphicsScene):
@@ -29,83 +19,22 @@ class GraphicsSceneDelegate(QGraphicsScene):
         stud_objs: Dict[int, GraphicsObjectDelegate] = {}
 
         for i in range(0, max(FrameSettings.FrameX.value, FrameSettings.FrameY.value)):
-            if (
-                i < FrameSettings.FrameX.value
-                and i % FrameSettings.StudSpacingX.value == 0
-            ):
-                stud_obj = GraphicsObjectDelegate()
-                scene.addItem(stud_obj)
-                stud_obj.setPos(i, 0)
-                stud_obj.setRect(
-                    0,
-                    StudSettings.StudX.value,
-                    StudSettings.StudX.value,
-                    FrameSettings.FrameY.value,
-                )
-                stud_objs[i] = stud_obj
-                stud_obj.mouseDoubleClickEvent = lambda event, stud_obj=stud_obj: GraphicsSceneDelegate.object_editor_dialog(
-                    stud_obj
-                )
-            if (
-                i < FrameSettings.FrameY.value
-                and i % FrameSettings.StudSpacingY.value == 0
-            ):
-                stud_obj = GraphicsObjectDelegate()
-                scene.addItem(stud_obj)
-                stud_obj.setPos(0, i)
-                stud_obj.setRect(
-                    0,
-                    0,
-                    FrameSettings.FrameX.value,
-                    StudSettings.StudX.value,
-                )
-                stud_objs[i] = stud_obj
-                stud_obj.mouseDoubleClickEvent = lambda event, stud_obj=stud_obj: GraphicsSceneDelegate.object_editor_dialog(
-                    stud_obj
-                )
-
-            # TODO: look up how to create singular dimensions and use rotation instead.
-
-    @staticmethod
-    def object_editor_dialog(stud_obj: GraphicsObjectDelegate) -> None:
-        """Open a dialog when a stud is pressed."""
-        dialog = QDialog()
-        dialog.setWindowTitle(OBJECT_EDITOR_DIALOG_TITLE)
-        dialog.setWindowIcon(QIcon(UI_ICON_PATH))
-
-        layout = QGridLayout()
-        dialog.setLayout(layout)
-
-        label = QLabel("Stud width:")
-
-        stud_size_spinbox = QDoubleSpinBox()
-        stud_size_spinbox.setRange(0, sys.float_info.max)
-        stud_size_spinbox.setSingleStep(1)
-        stud_size_spinbox.setDecimals(0)
-        stud_size_spinbox.setValue(stud_obj.rect().width())
-
-        save_button = QPushButton("Save")
-        discard_button = QPushButton("Discard")
-
-        save_button.clicked.connect(dialog.accept)
-        discard_button.clicked.connect(dialog.reject)
-
-        layout.addWidget(label, 0, 0)
-        layout.addWidget(stud_size_spinbox, 0, 1)
-        layout.addWidget(save_button, 1, 0)
-        layout.addWidget(discard_button, 1, 1)
-
-        dialog.setLayout(layout)
-        dialog.setMinimumSize(dialog.sizeHint())
-        dialog.setMaximumSize(dialog.sizeHint())
-
-        result = dialog.exec()
-
-        if result == QDialog.DialogCode.Accepted:
-            stud_obj.setRect(
-                0, 0, stud_size_spinbox.value(), FrameSettings.FrameY.value
-            )
-            # TODO: Or add here 2 different callbacks for the different axis.
-            print("Accepted")
-        elif result == QDialog.DialogCode.Rejected:
-            print("Rejected")
+            if i < FrameSettings.FrameX.value:
+                if i % FrameSettings.StudSpacingX.value == 0:
+                    stud_objs[i] = GraphicsObjectDelegate(
+                        i,
+                        StudSettings.StudX.value,
+                        StudSettings.StudX.value,
+                        FrameSettings.FrameY.value,
+                        0,
+                    )
+                    scene.addItem(stud_objs[i])
+                if i % FrameSettings.StudSpacingY.value == 0:
+                    stud_objs[i] = GraphicsObjectDelegate(
+                        0,
+                        i + StudSettings.StudX.value,
+                        StudSettings.StudX.value,
+                        FrameSettings.FrameX.value,
+                        -90,
+                    )
+                    scene.addItem(stud_objs[i])
