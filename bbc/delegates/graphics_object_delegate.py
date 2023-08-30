5ec804f892f69c6ee2f184715920e87bcc342bbe
Illyrius <FitimQ@live.nl>
2023-08-30 14:05:16 +0200

    -

--- bbc/delegates/graphics_object_delegate.py
+++ bbc/delegates/graphics_object_delegate.py
@@ -4,11 +4,12 @@ import sys
 from functools import partial
 
 from constants import OBJECT_EDITOR_DIALOG_TITLE, UI_ICON_PATH
-from enums.afc_enums import StudSettings
+from enums.afc_enums import LumberTypes, StudSettings
 from enums.q_enums import BrushStyleTypes, GraphicsItemFlagTypes, PenStyleTypes
 from handlers.dialog_handler import DialogHandler
 from PySide6.QtGui import QBrush, QColor, QIcon, QPen
 from PySide6.QtWidgets import (
+    QCheckBox,
     QComboBox,
     QDialog,
     QDoubleSpinBox,
@@ -31,6 +32,8 @@ class GraphicsObjectDelegate(QGraphicsRectItem):
         self._dialog_handler = DialogHandler()
         self.obj_id = GraphicsObjectDelegate.generate_complex_id()
         self.obj_props = {
+            "object_id": self.obj_id,
+            "type": StudSettings.Type.value,
             "x": x,
             "y": y,
             "w": w,
@@ -39,6 +42,7 @@ class GraphicsObjectDelegate(QGraphicsRectItem):
             "pen_color": StudSettings.PenColor.value,
             "pen_thickness": StudSettings.PenThickness.value,
             "pen_style": StudSettings.PenStyle.value,
+            "fill_state": StudSettings.Fill.value,
             "fill_color": StudSettings.FillColor.value,
             "fill_pattern": StudSettings.FillPattern.value,
             "fill_opacity": max(0, min(StudSettings.FillOpacity.value, 100)) / 100,
@@ -99,72 +103,74 @@ class GraphicsObjectDelegate(QGraphicsRectItem):
         dialog.setWindowTitle(OBJECT_EDITOR_DIALOG_TITLE)
         dialog.setWindowIcon(QIcon(UI_ICON_PATH))
 
-        layout = QGridLayout()
-        dialog.setLayout(layout)
+        layout = QGridLayout(dialog)
+        row_offset = 2
 
-        layout.addWidget(QLabel("Object ID:"), 0, 0)
-        layout.addWidget(QLabel(str(self.obj_id)), 0, 1)
-
-        layout.addWidget(QLabel("Tooltip:"), 1, 0)
-        layout.addWidget(QLabel(StudSettings.Type.value), 1, 1)
-
-        obj_props_labels = [
+        properties = [
+            "object_id",
+            "type",
             "pen_color",
             "pen_thickness",
             "pen_style",
+            "fill_state",
             "fill_color",
             "fill_pattern",
             "fill_opacity",
             "draw_order",
         ]
-        obj_props_inputs = {}
+        inputs = {}
 
-        row_offset = 2
-
-        for row, prop_name in enumerate(obj_props_labels):
+        for row, prop_name in enumerate(properties):
             label = QLabel(f"{prop_name.capitalize()}:")
             label.setWordWrap(True)
-            module = None
+
+            input_widget = None
 
             if prop_name in self.obj_props:
                 prop_value = self.obj_props[prop_name]
 
-                if isinstance(prop_value, str) and prop_value.startswith("#"):
-                    module = QPushButton(prop_value)
-                    module.clicked.connect(
+                if prop_name == "object_id":
+                    input_widget = QLabel()
+                    input_widget.setText(self.obj_id)
+                elif prop_name == "type":
+                    input_widget = QComboBox()
+                    input_widget.addItems([enum.name for enum in LumberTypes])
+                    input_widget.setCurrentText(prop_value)
+                elif isinstance(prop_value, str) and prop_value.startswith("#"):
+                    input_widget = QPushButton(prop_value)
+                    input_widget.clicked.connect(
                         partial(
                             self._dialog_handler.color_picker_dialog,
-                            button=module,
+                            button=input_widget,
                             properties=self.obj_props,
                             key=prop_name,
                         )
                     )
                 elif prop_name == "pen_style":
-                    module = QComboBox()
-                    enum_values = [enum.name for enum in PenStyleTypes]
-                    module.addItems(enum_values)
-                    if prop_value in enum_values:
-                        module.setCurrentText(prop_value)
+                    input_widget = QComboBox()
+                    input_widget.addItems([enum.name for enum in PenStyleTypes])
+                    input_widget.setCurrentText(prop_value)
                 elif prop_name == "fill_pattern":
-                    module = QComboBox()
-                    enum_values = [enum.name for enum in BrushStyleTypes]
-                    module.addItems(enum_values)
-                    if prop_value in enum_values:
-                        module.setCurrentText(prop_value)
+                    input_widget = QComboBox()
+                    input_widget.addItems([enum.name for enum in BrushStyleTypes])
+                    input_widget.setCurrentText(prop_value)
+                elif isinstance(prop_value, bool):
+                    input_widget = QCheckBox()
+                    input_widget.setChecked(prop_value)
                 elif isinstance(prop_value, int) or isinstance(prop_value, float):
-                    module = QDoubleSpinBox()
-                    module.setRange(0, sys.float_info.max)
-                    module.setSingleStep(1)
-                    module.setDecimals(0)
-                    module.setValue(prop_value)
+                    input_widget = QDoubleSpinBox()
+                    input_widget.setRange(0, sys.float_info.max)
+                    input_widget.setSingleStep(1)
+                    input_widget.setDecimals(0)
+                    input_widget.setValue(prop_value)
                 elif isinstance(prop_value, str):
-                    module = QLineEdit()
-                    module.setText(prop_value)
+                    input_widget = QLineEdit()
+                    input_widget.setText(prop_value)
 
-            if module:
-                obj_props_inputs[prop_name] = module
+            if input_widget is not None:
+                inputs[prop_name] = input_widget
                 layout.addWidget(label, row + row_offset, 0)
-                layout.addWidget(module, row + row_offset, 1)
+                layout.addWidget(input_widget, row + row_offset, 1)
 
         save_button = QPushButton("Save")
         discard_button = QPushButton("Discard")
@@ -172,19 +178,11 @@ class GraphicsObjectDelegate(QGraphicsRectItem):
         save_button.clicked.connect(dialog.accept)
         discard_button.clicked.connect(dialog.reject)
 
-        layout.addWidget(save_button, row_offset + len(obj_props_labels), 0, 1, 2)
-        layout.addWidget(
-            discard_button, row_offset + len(obj_props_labels) + 1, 0, 1, 2
-        )
+        layout.addWidget(save_button, row_offset + len(properties), 0, 1, 2)
+        layout.addWidget(discard_button, row_offset + len(properties) + 1, 0, 1, 2)
 
-        dialog.setLayout(layout)
-        dialog.setMinimumSize(dialog.sizeHint())
-        dialog.setMaximumSize(dialog.sizeHint())
-
-        result = dialog.exec()
-
-        if result == dialog.DialogCode.Accepted:
-            for prop_name, input_widget in obj_props_inputs.items():
+        if dialog.exec() == dialog.DialogCode.Accepted:
+            for prop_name, input_widget in inputs.items():
                 if isinstance(input_widget, QDoubleSpinBox):
                     self.obj_props[prop_name] = input_widget.value()
                 elif isinstance(input_widget, QLineEdit):
