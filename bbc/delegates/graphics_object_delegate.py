6234f5c4fba439814a0e33ac798c42f42d7b2ab0
Illyrius <FitimQ@live.nl>
2023-08-30 16:23:36 +0200

    -

--- bbc/delegates/graphics_object_delegate.py
+++ bbc/delegates/graphics_object_delegate.py
@@ -1,24 +1,11 @@
 import random
 import string
-import sys
-from functools import partial
 
-from constants import OBJECT_EDITOR_DIALOG_TITLE, UI_ICON_PATH
-from enums.afc_enums import LumberTypes, StudSettings
+from enums.afc_enums import StudSettings
 from enums.q_enums import BrushStyleTypes, GraphicsItemFlagTypes, PenStyleTypes
 from handlers.dialog_handler import DialogHandler
-from PySide6.QtGui import QBrush, QColor, QIcon, QPen
-from PySide6.QtWidgets import (
-    QCheckBox,
-    QComboBox,
-    QDialog,
-    QDoubleSpinBox,
-    QGraphicsRectItem,
-    QGridLayout,
-    QLabel,
-    QLineEdit,
-    QPushButton,
-)
+from PySide6.QtGui import QBrush, QColor, QPen
+from PySide6.QtWidgets import QGraphicsRectItem
 
 
 class GraphicsObjectDelegate(QGraphicsRectItem):
@@ -29,8 +16,7 @@ class GraphicsObjectDelegate(QGraphicsRectItem):
     def __init__(self, x: int, y: int, w: int, h: int, rad: int) -> None:
         """Initialize the graphics object delegate."""
         super().__init__()
-        self._dialog_handler = DialogHandler()
-        self.obj_id = GraphicsObjectDelegate.generate_complex_id()
+        self.obj_id = self.generate_complex_id()
         self.obj_props = {
             "object_id": self.obj_id,
             "type": StudSettings.Type.value,
@@ -49,10 +35,12 @@ class GraphicsObjectDelegate(QGraphicsRectItem):
             "draw_order": StudSettings.DrawOrder.value,
         }
         GraphicsObjectDelegate.obj_counter += 1
+
+        self._dialog_handler = DialogHandler()
+
         self.setup_graphics_object(x, y, w, h, rad)
 
-    @staticmethod
-    def generate_complex_id(char_len=6) -> str:
+    def generate_complex_id(self, char_len=6) -> str:
         """Generate a complex ID."""
         chars = string.ascii_letters + string.digits
         return "".join(random.choice(chars) for _ in range(char_len))
@@ -74,7 +62,11 @@ class GraphicsObjectDelegate(QGraphicsRectItem):
         self.setPos(x, y)
         self.setRect(self.rect().x(), self.rect().y(), w, h)
         self.setRotation(rad)
-        self.mouseDoubleClickEvent = partial(self.object_editor_dialog)
+        self.mouseDoubleClickEvent = (
+            lambda event: self._dialog_handler.object_editor_dialog(
+                self.obj_id, self.obj_props
+            )
+        )
 
         self.setPen(
             QPen(
@@ -96,102 +88,3 @@ class GraphicsObjectDelegate(QGraphicsRectItem):
 
         self.setZValue(StudSettings.DrawOrder.value)
         self.setToolTip(StudSettings.Type.value)
-
-    def object_editor_dialog(self, event) -> None:
-        """Open a dialog when an object is pressed."""
-        dialog = QDialog()
-        dialog.setWindowTitle(OBJECT_EDITOR_DIALOG_TITLE)
-        dialog.setWindowIcon(QIcon(UI_ICON_PATH))
-
-        layout = QGridLayout(dialog)
-        row_offset = 2
-
-        properties = [
-            "object_id",
-            "type",
-            "pen_color",
-            "pen_thickness",
-            "pen_style",
-            "fill_state",
-            "fill_color",
-            "fill_pattern",
-            "fill_opacity",
-            "draw_order",
-        ]
-        inputs = {}
-
-        for row, prop_name in enumerate(properties):
-            label = QLabel(f"{prop_name.capitalize()}:")
-            label.setWordWrap(True)
-
-            input_widget = None
-
-            if prop_name in self.obj_props:
-                prop_value = self.obj_props[prop_name]
-
-                if prop_name == "object_id":
-                    input_widget = QLabel()
-                    input_widget.setText(self.obj_id)
-                elif prop_name == "type":
-                    input_widget = QComboBox()
-                    input_widget.addItems([enum.name for enum in LumberTypes])
-                    input_widget.setCurrentText(prop_value)
-                elif isinstance(prop_value, str) and prop_value.startswith("#"):
-                    input_widget = QPushButton(prop_value)
-                    input_widget.clicked.connect(
-                        partial(
-                            self._dialog_handler.color_picker_dialog,
-                            button=input_widget,
-                            properties=self.obj_props,
-                            key=prop_name,
-                        )
-                    )
-                elif prop_name == "pen_style":
-                    input_widget = QComboBox()
-                    input_widget.addItems([enum.name for enum in PenStyleTypes])
-                    input_widget.setCurrentText(prop_value)
-                elif prop_name == "fill_pattern":
-                    input_widget = QComboBox()
-                    input_widget.addItems([enum.name for enum in BrushStyleTypes])
-                    input_widget.setCurrentText(prop_value)
-                elif isinstance(prop_value, bool):
-                    input_widget = QCheckBox()
-                    input_widget.setChecked(prop_value)
-                elif isinstance(prop_value, int) or isinstance(prop_value, float):
-                    input_widget = QDoubleSpinBox()
-                    input_widget.setRange(0, sys.float_info.max)
-                    input_widget.setSingleStep(1)
-                    input_widget.setDecimals(0)
-                    input_widget.setValue(prop_value)
-                elif isinstance(prop_value, str):
-                    input_widget = QLineEdit()
-                    input_widget.setText(prop_value)
-
-            if input_widget is not None:
-                inputs[prop_name] = input_widget
-                layout.addWidget(label, row + row_offset, 0)
-                layout.addWidget(input_widget, row + row_offset, 1)
-
-        save_button = QPushButton("Save")
-        discard_button = QPushButton("Discard")
-
-        save_button.clicked.connect(dialog.accept)
-        discard_button.clicked.connect(dialog.reject)
-
-        layout.addWidget(save_button, row_offset + len(properties), 0, 1, 2)
-        layout.addWidget(discard_button, row_offset + len(properties) + 1, 0, 1, 2)
-
-        if dialog.exec() == dialog.DialogCode.Accepted:
-            for prop_name, input_widget in inputs.items():
-                if isinstance(input_widget, QDoubleSpinBox):
-                    self.obj_props[prop_name] = input_widget.value()
-                elif isinstance(input_widget, QLineEdit):
-                    self.obj_props[prop_name] = input_widget.text()
-
-            self.setRect(
-                self.obj_props["x"],
-                self.obj_props["y"],
-                self.obj_props["w"],
-                self.obj_props["h"],
-            )
-            self.setRotation(self.obj_props["rad"])
--- bbc/handlers/dialog_handler.py
+++ bbc/handlers/dialog_handler.py
@@ -1,21 +1,35 @@
+import sys
+from functools import partial
 from typing import Dict
 
-from constants import COLOR_PICKER_DIALOG_TITLE, QUIT_DIALOG_TITLE, UI_ICON_PATH
-from PySide6.QtGui import QColor, QIcon, QPalette
+from constants import (
+    COLOR_PICKER_DIALOG_TITLE,
+    OBJECT_EDITOR_DIALOG_TITLE,
+    QUIT_DIALOG_TITLE,
+    UI_ICON_PATH,
+)
+from enums.afc_enums import LumberTypes
+from enums.q_enums import BrushStyleTypes, PenStyleTypes
+from PySide6.QtGui import QColor, QFont, QIcon, QPalette
 from PySide6.QtWidgets import (
+    QAbstractSpinBox,
     QApplication,
+    QCheckBox,
     QColorDialog,
+    QComboBox,
     QDialog,
+    QDoubleSpinBox,
+    QFrame,
+    QGridLayout,
+    QLabel,
     QMessageBox,
     QPushButton,
+    QWidget,
 )
 
 
 class DialogHandler:
     """A class to handle dialogs."""
-
-    def __init__(self):
-        """Initialize the class."""
 
     @staticmethod
     def quit_dialog(quit_application: bool) -> None:
@@ -44,6 +58,203 @@ class DialogHandler:
                 QApplication.quit()
             else:
                 QApplication.activeWindow().close()
+
+    @staticmethod
+    def object_editor_dialog(obj_id: str, obj_props: dict) -> None:
+        """Open a dialog when an object is pressed."""
+        _dialog_handler = DialogHandler()
+
+        dialog = QDialog()
+        dialog.setWindowTitle(OBJECT_EDITOR_DIALOG_TITLE)
+        dialog.setWindowIcon(QIcon(UI_ICON_PATH))
+        dialog.setFixedSize(dialog.sizeHint())
+
+        layout = QGridLayout(dialog)
+
+        props = [
+            {
+                "frame_title": "General Settings:",
+                "frame_border": QFrame.Shape.Box,
+                "data": [
+                    [
+                        {
+                            "widget": QLabel(),
+                            "content": "Object ID:",
+                        },
+                        {
+                            "widget": QLabel(),
+                            "content": obj_id,
+                        },
+                    ],
+                    [
+                        {
+                            "widget": QLabel(),
+                            "content": "Type:",
+                        },
+                        {
+                            "widget": QComboBox(),
+                            "content": obj_props["type"],
+                            "content_options": [enum.name for enum in LumberTypes],
+                        },
+                    ],
+                    [
+                        {
+                            "widget": QLabel(),
+                            "content": "Draw Order:",
+                        },
+                        {
+                            "widget": QDoubleSpinBox(),
+                            "content": obj_props["draw_order"],
+                        },
+                    ],
+                ],
+            },
+            {
+                "frame_title": "Annotation Settings:",
+                "frame_border": QFrame.Shape.Box,
+                "data": [
+                    [
+                        {
+                            "widget": QLabel(),
+                            "content": "Pen Color:",
+                        },
+                        {
+                            "widget": QPushButton(),
+                            "content": obj_props["pen_color"],
+                        },
+                    ],
+                    [
+                        {
+                            "widget": QLabel(),
+                            "content": "Pen Thickness:",
+                        },
+                        {
+                            "widget": QDoubleSpinBox(),
+                            "content": obj_props["pen_thickness"],
+                        },
+                    ],
+                    [
+                        {
+                            "widget": QLabel(),
+                            "content": "Pen Style:",
+                        },
+                        {
+                            "widget": QComboBox(),
+                            "content": obj_props["pen_style"],
+                            "content_options": [enum.name for enum in PenStyleTypes],
+                        },
+                    ],
+                    [
+                        {
+                            "widget": QLabel(),
+                            "content": "Fill State:",
+                        },
+                        {
+                            "widget": QCheckBox(),
+                            "content": obj_props["fill_state"],
+                        },
+                    ],
+                    [
+                        {
+                            "widget": QLabel(),
+                            "content": "Fill Color:",
+                        },
+                        {
+                            "widget": QPushButton(),
+                            "content": QPushButton(obj_props["fill_color"]),
+                        },
+                    ],
+                    [
+                        {
+                            "widget": QLabel(),
+                            "content": "Fill Pattern:",
+                        },
+                        {
+                            "widget": QComboBox(),
+                            "content": obj_props["fill_pattern"],
+                            "content_options": [enum.name for enum in BrushStyleTypes],
+                        },
+                    ],
+                    [
+                        {
+                            "widget": QLabel(),
+                            "content": "Fill Opacity:",
+                        },
+                        {
+                            "widget": QDoubleSpinBox(),
+                            "content": obj_props["fill_opacity"],
+                        },
+                    ],
+                ],
+            },
+        ]
+
+        inputs = {}
+
+        save_button = QPushButton("Save")
+        discard_button = QPushButton("Discard")
+
+        save_button.clicked.connect(dialog.accept)
+        discard_button.clicked.connect(dialog.reject)
+
+        row = 0
+        for prop_group in props:
+            frame_title_widget = QLabel()
+            frame_title_widget.setText(prop_group["frame_title"])
+            frame_title_widget.setFrameShape(prop_group["frame_border"])
+
+            bold_font = QFont()
+            bold_font.setBold(True)
+
+            frame_title_widget.setFont(bold_font)
+
+            layout.addWidget(frame_title_widget, row, 0, 1, 2)
+
+            row += 1
+
+            for prop_data in prop_group["data"]:
+                label_widget: QLabel = prop_data[0]["widget"]
+                label_widget.setText(prop_data[0]["content"])
+                layout.addWidget(label_widget, row, 0)
+
+                input_widget: QWidget = prop_data[1]["widget"]
+                input_content = prop_data[1]["content"]
+                if isinstance(input_widget, QPushButton):
+                    input_widget.clicked.connect(
+                        partial(
+                            _dialog_handler.color_picker_dialog,
+                            button=input_widget,
+                            properties=obj_props,
+                            key=prop_data[0]["content"],
+                        )
+                    )
+                if isinstance(input_widget, QCheckBox):
+                    input_widget.setChecked(input_content)
+                elif isinstance(input_widget, QComboBox):
+                    input_widget.addItems(prop_data[1]["content_options"])
+                    input_widget.setCurrentText(input_content)
+                elif isinstance(input_widget, QDoubleSpinBox):
+                    input_widget.setRange(0, sys.float_info.max)
+                    input_widget.setSingleStep(1)
+                    input_widget.setDecimals(0)
+                    input_widget.setValue(input_content)
+                    input_widget.setButtonSymbols(
+                        QAbstractSpinBox.ButtonSymbols.NoButtons
+                    )
+                elif isinstance(input_widget, QLabel):
+                    input_widget.setText(input_content)
+                layout.addWidget(input_widget, row, 1)
+
+                inputs[prop_data[0]["content"]] = input_widget
+                row += 1
+
+        layout.addWidget(save_button, row, 0, 1, 2)
+        row += 1
+        layout.addWidget(discard_button, row, 0, 1, 2)
+
+        if dialog.exec() == dialog.DialogCode.Accepted:
+            for prop_name, input_widget in inputs.items():
+                obj_props[prop_name] = input_widget.getContent()
 
     @staticmethod
     def color_picker_dialog(button: QPushButton, properties: Dict, key: str):
