5ba315b406cfbb64a37d6e14f843088c48e25e4a
Illyrius <FitimQ@live.nl>
2023-09-04 13:35:07 +0200

    added settings_manager as helper

--- bbc/helpers/helper.py
+++ bbc/helpers/helper.py
@@ -1,17 +1,32 @@
 import random
 import string
+from typing import Optional
+
+from PySide6.QtCore import QSettings
 
 
 class Helper:
     """Helper class for common functions"""
 
     @staticmethod
-    def generate_complex_id(char_len=6) -> str:
+    def generate_complex_id(char_len: int = 6) -> str:
         """Generate a complex ID."""
         chars = string.ascii_letters + string.digits
         return "".join(random.choice(chars) for _ in range(char_len))
 
     @staticmethod
-    def get_data_from_app(app, key) -> str:
-        """Get data from the application."""
-        return app.data[key]
+    def settings_manager(
+        key: str, value: str, group: Optional[str] = None
+    ) -> Optional[str]:
+        """Manage the settings."""
+        settings = QSettings()
+        if group:
+            settings.beginGroup(group)
+
+        settings.setValue(key, value)
+        settings.sync()
+
+        if group:
+            settings.endGroup()
+
+        return str(settings.value(key))
