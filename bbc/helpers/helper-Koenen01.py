f5e64b2b4ce8d82d318bb164d5223a3f2453805c
Illyrius <FitimQ@live.nl>
2023-09-11 10:37:02 +0200

    -

---
+++ bbc/helpers/helper-Koenen01.py
@@ -0,0 +1,127 @@
+import random
+import string
+from typing import Optional
+
+from enums.q_enums import AspectRatioModeTypes, ModuleType
+from ezdxf import filemanagement as fm
+from ezdxf.lldxf import const
+from PySide6.QtCore import QPoint, QSettings
+from PySide6.QtWidgets import QApplication, QGraphicsView, QWidget
+
+
+class Helper:
+    """Helper class for common functions"""
+
+    @staticmethod
+    def complex_id_generator(char_len: int = 6) -> str:
+        """Generate a complex ID."""
+        chars = string.ascii_letters + string.digits
+        return "".join(random.choice(chars) for _ in range(char_len))
+
+    @staticmethod
+    def settings_manager(
+        key: str, value: str, group: Optional[str] = None
+    ) -> Optional[str]:
+        """Manage the settings."""
+        settings = QSettings()
+        if group:
+            settings.beginGroup(group)
+
+        settings.setValue(key, value)
+        settings.sync()
+
+        if group:
+            settings.endGroup()
+
+        return str(settings.value(key))
+
+    @staticmethod
+    def center_ui_on_screen(ui: QWidget) -> None:
+        """Centers the window on the primary screen."""
+        screen_center = QApplication.primaryScreen().geometry().center()
+        ui_size = ui.size()
+        ui.move(screen_center - QPoint(ui_size.width() // 2, ui_size.height() // 2))
+
+    @staticmethod
+    def set_ui_size(ui: QWidget, size: Optional[tuple[int, int]] = None) -> None:
+        """Sets the size of the UI."""
+        if size is None:
+            ui.resize(ui.sizeHint())
+        else:
+            width, height = size
+            ui.resize(width, height)
+
+    @staticmethod
+    def toggle_ui_visibility(uis: list[QWidget]) -> None:
+        """Toggles the visibility of UI(s)."""
+        for ui in uis:
+            ui.setVisible(not ui.isVisible())
+            if ui.isVisible():
+                Helper.center_ui_on_screen(ui)
+
+    @staticmethod
+    def switch_modules(module: ModuleType.StackedWidget.value) -> None:
+        """Switches the modules."""
+        current_index = module.currentIndex()
+        new_index = (current_index + 1) % module.count()
+        module.setCurrentIndex(new_index)
+
+    @staticmethod
+    def fit_scene_in_view(instance: QGraphicsView) -> None:
+        """Fits the scene in the view."""
+        instance.fitInView(
+            instance.sceneRect(), AspectRatioModeTypes.KeepAspectRatio.value
+        )
+
+    @staticmethod
+    def read_dwg_file(file_path: str) -> list[dict]:
+        """Reads a DWG file and returns the entity information."""
+        entity_info = []
+        try:
+            doc = fm.readfile(file_path)
+            msp = doc.modelspace()
+
+            for entity in msp.query("*"):
+                entity_type = entity.dxftype()
+                layer_name = entity.dxf.layer
+
+                properties = []
+
+                if entity_type == "LINE":
+                    properties = {
+                        "Entity Type": entity_type,
+                        "Layer": layer_name,
+                        "Start Point": entity.dxf.start,
+                        "End Point": entity.dxf.end,
+                        "Color": entity.dxf.color,
+                        "Line Type": entity.dxf.linetype,
+                        "Line Weight": entity.dxf.lineweight,
+                    }
+                elif entity_type in ("TEXT", "MTEXT"):
+                    properties = {
+                        "Entity Type": entity_type,
+                        "Layer": layer_name,
+                        "Text Content": entity.dxf.text,
+                        "Color": entity.dxf.color,
+                        "Line Type": entity.dxf.linetype,
+                        "Line Weight": entity.dxf.lineweight,
+                    }
+                elif entity_type == "CIRCLE":
+                    properties = {
+                        "Entity Type": entity_type,
+                        "Layer": layer_name,
+                        "Center": entity.dxf.center,
+                        "Radius": entity.dxf.radius,
+                        "Color": entity.dxf.color,
+                        "Line Type": entity.dxf.linetype,
+                        "Line Weight": entity.dxf.lineweight,
+                    }
+
+                entity_info.append(properties)
+
+        except const.DXFError as e:
+            print(f"Error reading DWG file: {e}")
+
+        return entity_info
+
+    # TODO: add method that sends back the properties after editing.
---
+++ bbc/utils/module_handler-Koenen01.py
@@ -0,0 +1,152 @@
+from enum import Enum
+from typing import Any, Callable, Optional
+
+from delegates.checkbox_delegate import CheckBoxDelegate
+from delegates.color_dialog_delegate import ColorDialogDelegate
+from delegates.doublespinbox_delegate import DoubleSpinBoxDelegate
+from delegates.graphics_view_delegate import GraphicsViewDelegate
+from delegates.label_delegate import LabelDelegate
+from delegates.lineedit_delegate import LineEditDelegate
+from delegates.message_box_delegate import MessageBoxDelegate
+from delegates.pushbutton_delegate import PushButtonDelegate, QPushButton
+from enums.module_enums import (
+    Checkboxes,
+    ColorDialogs,
+    DoubleSpinBoxes,
+    GraphicsViews,
+    Labels,
+    LineEdits,
+    MessageBoxes,
+    PushButtons,
+)
+from enums.q_enums import AlignmentType, LayoutType, SizePolicyType
+from PySide6.QtCore import Slot
+from PySide6.QtWidgets import QWidget
+
+
+class ModuleHandler(QWidget):
+    """A class to handle the modules."""
+
+    def __init__(
+        self,
+        matrix_name: Enum,
+        matrix_margins: Optional[tuple[int, int, int, int]] = None,
+    ) -> None:
+        """Initialize the ModuleHandler."""
+        super().__init__()
+        self._module_visibility = {}
+        self._module_mapping = {}
+
+        self.create_modules_from_matrix(matrix_name, matrix_margins)
+
+    def create_modules_from_matrix(
+        self,
+        matrix_name: Enum,
+        matrix_margins: Optional[tuple[int, int, int, int]] = None,
+    ):
+        """Create modules from a matrix."""
+        if isinstance(matrix_name, Enum):
+            matrix_data: list[list[tuple[Any, ...]]] = matrix_name.value
+        else:
+            matrix_data: list[list[tuple[Any, ...]]] = matrix_name
+
+        if not isinstance(matrix_data, list) or not all(
+            isinstance(row, list) for row in matrix_data
+        ):
+            raise TypeError("Invalid matrix data")
+
+        if matrix_data:
+            layout = LayoutType.Grid.value(self)
+            layout.setContentsMargins(*self.setup_module_margins(self, matrix_margins))
+
+            for row, row_modules in enumerate(matrix_data):
+                for column, module_data in enumerate(row_modules):
+                    layout.addWidget(
+                        self.setup_module_container(*module_data), row, column
+                    )
+
+    def setup_module_container(
+        self,
+        module_layout_type: LayoutType,
+        module_enum: type[Enum],
+        module_margins: Optional[tuple[int, int, int, int]],
+        module_alignment: Optional[AlignmentType],
+        module_size_policy: Optional[tuple[SizePolicyType, SizePolicyType]],
+    ) -> QWidget:
+        """Setup the module container layout."""
+        module_container = QWidget()
+
+        _, delegate = self.setup_module(module_enum)
+
+        if delegate:
+            self._module_mapping[module_enum.name] = delegate
+
+            layout = module_layout_type.value(module_container)
+            layout.setContentsMargins(*self.setup_module_margins(self, module_margins))
+            if module_alignment is not None:
+                layout.setAlignment(module_alignment.value)
+
+            if module_size_policy is not None:
+                size_policy_x, size_policy_y = module_size_policy
+                delegate.setSizePolicy(size_policy_x.value, size_policy_y.value)
+
+            layout.addWidget(delegate)
+
+        return module_container
+
+    @staticmethod
+    def setup_module(
+        module_enum: type[Enum],
+    ) -> tuple[Optional[dict[str, Any]], Optional[QWidget]]:
+        """Setup the module data and properties."""
+        delegate_class = {
+            Labels: LabelDelegate,
+            Checkboxes: CheckBoxDelegate,
+            LineEdits: LineEditDelegate,
+            DoubleSpinBoxes: DoubleSpinBoxDelegate,
+            PushButtons: PushButtonDelegate,
+            GraphicsViews: GraphicsViewDelegate,
+            MessageBoxes: MessageBoxDelegate,
+            ColorDialogs: ColorDialogDelegate,
+        }.get(type(module_enum))
+
+        return (
+            module_enum.value,  # type: ignore # TODO: find out why the typing is bugging out. could be an external factor.
+            delegate_class(module_enum.value) if delegate_class else None,
+        )
+
+    @staticmethod
+    def setup_module_margins(
+        parent: QWidget, module_margins: Optional[tuple[int, int, int, int]]
+    ) -> tuple[int, int, int, int]:
+        """Set the margins of the layout."""
+        if module_margins is None:
+            parent_margins = parent.contentsMargins()
+            return (
+                parent_margins.left(),
+                parent_margins.top(),
+                parent_margins.right(),
+                parent_margins.bottom(),
+            )
+        return module_margins
+
+    def module_connection(self, module_enum: Enum, target_method: Callable) -> None:
+        """Connect the module signal to the target method."""
+        module_reference = self._module_mapping.get(module_enum.name)
+        if isinstance(module_reference, QPushButton):
+            module_reference.clicked.connect(target_method)
+        else:
+            raise ValueError(
+                f"{module_enum}: not found or not supported for module connection"
+            )
+
+    @Slot(str, str)
+    def toggle_module_visibility(self, module_enum: Enum) -> None:
+        """Toggle the module visibility."""
+        module_reference = self._module_mapping.get(module_enum.name)
+        if isinstance(module_reference, QWidget):
+            module_reference.setVisible(not module_reference.isVisible())
+        else:
+            raise ValueError(
+                f"{module_enum}: not found or not supported for visibility toggling"
+            )
