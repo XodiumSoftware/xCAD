9939c3d2a277e1fbb92024d8aeb9c1ecaa098cc3
Illyrius <FitimQ@live.nl>
2023-10-26 16:58:18 +0200

    -

--- src/modules/tool_bar_module.py
+++ src/modules/tool_bar_module.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Optional
 
 from PySide6.QtGui import QAction
 from PySide6.QtWidgets import QToolBar
@@ -7,7 +7,7 @@ from PySide6.QtWidgets import QToolBar
 class ToolBarModule(QToolBar):
     """A class used to represent a toolbar module."""
 
-    def __init__(self, data: str, action: List[str]) -> None:
+    def __init__(self, data: str, action: Optional[List[str]] = None) -> None:
         """Initialize the class.
 
         Args:
@@ -17,21 +17,22 @@ class ToolBarModule(QToolBar):
         super().__init__()
         self.setup_props(data, action)
 
-    def setup_props(self, data: str, action: List[str]) -> None:  # TODO
+    def setup_props(
+        self, data: str, action: Optional[List[str]] = None
+    ) -> None:  # TODO
         """Setup the properties.
 
         Args:
             data (str): A configuration.
             action_names (List[str]): List of action names.
         """
-        for name in action:
-            action = QAction(name, self)
-            self.addAction(action)
+        self.setWindowTitle(data)
 
-            # Customize properties of the actions (optional)
-            # action.setIcon(QIcon("icon.png"))
-            # action.setToolTip("Tooltip text")
+        if action:
+            for name in action:
+                action = QAction(name, self)
+                self.addAction(action)
 
-        # Customize properties of the toolbars (optional)
-        # self.setOrientation(Qt.Vertical)
-        # self.setMovable(False)
+                # Customize properties of the actions (optional)
+                # action.setIcon(QIcon("icon.png"))
+                # action.setToolTip("Tooltip text")
