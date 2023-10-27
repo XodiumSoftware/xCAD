8729c41ea8ef10b133dfc1793ad06fe195c0078b
Illyrius <FitimQ@live.nl>
2023-10-27 15:55:51 +0200

    -

--- src/handlers/module_handler.py
+++ src/handlers/module_handler.py
@@ -1,13 +1,10 @@
-from enum import Enum
-from typing import List
-
-from PySide6.QtWidgets import QGridLayout, QGroupBox, QLayout, QWidget
+from PySide6.QtWidgets import QGridLayout, QWidget
 
 
 class MatrixHandler(QWidget):
     """A class to handle the matrices."""
 
-    def __init__(self, matrix: Enum) -> None:
+    def __init__(self, matrix) -> None:
         """Initialize the class.
 
         Args:
@@ -17,44 +14,12 @@ class MatrixHandler(QWidget):
 
         self.matrix(matrix)
 
-    def matrix(self, matrix: Enum) -> None:
+    def matrix(self, matrix) -> None:
         """Handle the matrix.
 
         Args:
             matrix (Enum): A matrix.
         """
-        matrix_data: List[List[Enum]] = matrix.value
         layout = QGridLayout()
 
-        for row, row_modules in enumerate(matrix_data):
-            for column, module_data in enumerate(row_modules):
-                if isinstance(module_data, type) and issubclass(module_data, QGroupBox):
-                    layout.addWidget(self.container(self.module()), row, column)
-                else:
-                    layout.addWidget(self.module(), row, column)
-
         self.setLayout(layout)
-
-    @staticmethod
-    def container(module: QWidget, layout: QLayout = QGridLayout()) -> QGroupBox:
-        """Handle the container.
-
-        Args:
-            layout (QLayout, optional): A layout. Defaults to QGridLayout().
-
-        Returns:
-            QGroupBox: A container widget.
-        """
-        container = QGroupBox()
-        container.setLayout(layout)
-        layout.addWidget(module)
-        return container
-
-    @staticmethod
-    def module() -> QWidget:
-        """Handle the module.
-
-        Returns:
-            QWidget: A module widget.
-        """
-        return QWidget()
--- src/interfaces/modules/main_ui_module.py
+++ src/interfaces/modules/main_ui_module.py
@@ -1,11 +1,11 @@
 import qdarktheme
 from PySide6.QtGui import QIcon
-from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QMainWindow, QStackedWidget
 
 from handlers.events_handler import EventsHandler
-from interfaces.configs.label_configs import CopyrightLabelConfig
+from handlers.module_handler import MatrixHandler
+from interfaces.configs.matrix_configs import MainUIMatrixConfig
 from interfaces.configs.ui_configs import MainUIConfig
-from interfaces.modules.label_module import LabelModule
 from interfaces.modules.status_bar_module import StatusBarModule
 from interfaces.modules.tool_bar_module import ToolBarModule
 
@@ -18,10 +18,7 @@ class MainUIModule(QMainWindow):
         super().__init__()
         qdarktheme.setup_theme("auto")
 
-        # self._main_module_0, self._main_module_1 = (
-        #     MatrixHandler(MainUIMatrixConfig),
-        #     MatrixHandler(Matrices.MainMatrix1),
-        # )
+        self._main_module_0 = MatrixHandler(MainUIMatrixConfig)
 
         self._events_handler = EventsHandler()
 
@@ -30,10 +27,9 @@ class MainUIModule(QMainWindow):
     def setup_ui(self) -> None:
         """Setup the UIs."""
         self.setup_window()
-        self.layout().addWidget(LabelModule(CopyrightLabelConfig()))
         self.setStatusBar(StatusBarModule(MainUIConfig.STATUSBAR))
         self.addToolBar(ToolBarModule(MainUIConfig.TOOLBAR))
-        # self.setup_central_widget()
+        self.setup_central_widget()
         self.setup_visibility()
         self._events_handler.quit_on_key_press_event(self)
 
@@ -44,30 +40,12 @@ class MainUIModule(QMainWindow):
         self.resize(*MainUIConfig.INIT_SIZE)
         self.setContentsMargins(*MainUIConfig.MARGINS)
 
-    # def setup_central_widget(self) -> None:
-    #     """Set up the central widget."""
-    #     self._main_modules_stack = QStackedWidget(self)
-    #     self._main_modules_stack.addWidget(self._main_module_0)
-    #     self._main_modules_stack.addWidget(self._main_module_1)
-    #     self.setCentralWidget(self._main_modules_stack)
+    def setup_central_widget(self) -> None:
+        """Set up the central widget."""
+        self._main_modules_stack = QStackedWidget(self)
+        self._main_modules_stack.addWidget(self._main_module_0)
+        self.setCentralWidget(self._main_modules_stack)
 
     def setup_visibility(self) -> None:
         """Set up initial visibility."""
         self.show() if MainUIConfig.INIT_VISIBILITY else self.hide()
-
-    # def center_ui_on_screen(self) -> None:
-    #     """Centers a QWidget on the screen."""
-    #     screen_center: QPoint = QApplication.primaryScreen().geometry().center()
-    #     widget_center: QPoint = QPoint(
-    #         self.size().width() // 2, self.size().height() // 2
-    #     )
-    #     self.move(screen_center - widget_center)
-
-    # def toggle_ui_visibility(self, uis: List[QWidget]) -> bool:
-    #     """Toggles the visibility of a list of QWidgets."""
-    #     visibility_changed = any(ui.isHidden() for ui in uis)
-    #     for ui in uis:
-    #         ui.setVisible(not ui.isVisible())
-    #     if visibility_changed:
-    #         self.center_ui_on_screen()
-    #     return visibility_changed
