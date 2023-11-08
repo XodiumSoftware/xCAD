a41e63d521c9bb013452159c2a25b277a94673bd
Illyrius <FitimQ@live.nl>
2023-11-08 14:36:52 +0100

    Update Python version to 3.12.0

--- scripts/setup_python.ps1
+++ scripts/setup_python.ps1
@@ -5,7 +5,7 @@ param (
 $pythonPath = Get-Command python -ErrorAction SilentlyContinue
 if ($null -eq $pythonPath) {
     Write-Host -NoNewline "$prefix " -ForegroundColor Red; Write-Host "Python is not installed. Installing Python..."
-    Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.7/python-3.9.7-amd64.exe" -OutFile "python-installer.exe"
+    Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe" -OutFile "python-installer.exe"
     Start-Process -FilePath "python-installer.exe" -ArgumentList "/quiet InstallAllUsers=1 PrependPath=1 Include_test=0" -Wait
     Remove-Item -Path "python-installer.exe"
     Write-Host -NoNewline "$prefix " -ForegroundColor Red; Write-Host "Python installation complete!"
