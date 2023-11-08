c26a11d0f69667ede982e2a92beadbf600c25c1e
Illyrius <FitimQ@live.nl>
2023-11-08 13:22:37 +0100

    Add check for gcc installation and install if necessary in setup_msys2.bat

--- scripts/setup_msys2.bat
+++ scripts/setup_msys2.bat
@@ -4,7 +4,7 @@ rem Check if MSYS2 is already installed
 if exist C:\msys64 (
     echo MSYS2 is already installed.
     pause
-    goto end
+    goto gcc_install
 )
 
 echo Installing MSYS2...
@@ -12,11 +12,13 @@ curl -O "https://repo.msys2.org/distrib/x86_64/msys2-x86_64-latest.exe"
 msys2-x86_64-latest.exe /S
 echo MSYS2 installation complete!
 
+:gcc_install
+
 echo Checking if mingw-w64-ucrt-x86_64-gcc is installed...
-call C:\msys64\msys2_shell.cmd -mingw64 -no-start -defterm -exec="pacman -Q mingw-w64-ucrt-x86_64-gcc"
+call C:\msys64\msys2_shell.cmd -mingw64 -no-start -defterm -here -c "pacman -Q mingw-w64-ucrt-x86_64-gcc"
 if errorlevel 1 (
     echo Installing mingw-w64-ucrt-x86_64-gcc...
-    call C:\msys64\msys2_shell.cmd -mingw64 -no-start -defterm -exec="pacman -S mingw-w64-ucrt-x86_64-gcc"
+    call C:\msys64\msys2_shell.cmd -mingw64 -no-start -defterm -here -c "pacman -S mingw-w64-ucrt-x86_64-gcc"
     echo mingw-w64-ucrt-x86_64-gcc installation complete!
 ) else (
     echo mingw-w64-ucrt-x86_64-gcc is already installed.
