02ab4b1ec5b3fab5368eb6451251bcd2a002e240
Illyrius <28700752+illyrius666@users.noreply.github.com>
2023-11-16 12:32:35 +0100

    -

--- scripts/generators/unittest_generator.py
+++ scripts/generators/unittest_generator.py
@@ -7,20 +7,22 @@ def generate_unittest_file(dir_path, test_name, test_content):
         file.write("import unittest\n\n")
         file.write(test_content)
 
+
 def main():
-    src_dir = './src'
-    for root, dirs, files in os.walk(src_dir):
+    src_dir = "./src"
+    for root, _, files in os.walk(src_dir):
         for file in files:
-            if file.endswith('.py') and '__' not in file:
+            if file.endswith(".py") and "__" not in file:
                 test_name = os.path.splitext(file)[0]
                 test_content = f"""
 class Test{test_name.capitalize()}(unittest.TestCase):
     def test_case1(self):
         self.assertEqual(1, 1)
 """
-                test_dir = os.path.join('tests', os.path.relpath(root, src_dir))
+                test_dir = os.path.join("tests", os.path.relpath(root, src_dir))
                 os.makedirs(test_dir, exist_ok=True)
                 generate_unittest_file(test_dir, test_name, test_content)
+
 
 if __name__ == "__main__":
     main()
