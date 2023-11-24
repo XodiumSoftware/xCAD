410c8349b88e40938375369e082716fe00a941d4
Illyrius <28700752+illyrius666@users.noreply.github.com>
2023-11-24 14:39:55 +0100

    Deleted unnecessary files and scripts

--- scripts/setup_unittests.ps1
+++
@@ -1,8 +0,0 @@
-try {
-    Write-PrefixMsg "Generating unit test files..."
-    python .\scripts\generators\unittest_generator.py
-    Write-PrefixMsg "Unittest generation complete!"
-}
-catch {
-    Write-Error $_
-}
