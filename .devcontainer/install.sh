548c327355239e863d6b1dcdfb48db5f47f24629
illyrius666 <FitimQ@live.nl>
2023-12-20 15:31:01 +0000

    -

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,7 +1,12 @@
 pip install --upgrade pip
 pip install -r requirements.txt
 
-zip_file=".temp/BRXSDK_Bcad_V24_1_05.zip"
+source_dir="https://cloud.structura-engineering.com/storage/BRXSDK/BRXSDK_Bcad_V24_1_05.zip"
+temp_dir="/tmp/BRXSDK/BRXSDK_Bcad_V24_1_05.zip"
 dest_dir="/usr/local/lib/BRXSDK"
 
-sudo unzip $zip_file -d $dest_dir
+mkdir -p $(dirname $temp_dir)
+curl $source_dir -o $temp_dir
+
+sudo unzip -o $temp_dir -d $dest_dir
+rm $temp_dir
