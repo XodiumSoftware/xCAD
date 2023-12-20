85be2dbb05b87360e45e3f9b58ffccf0f5fd943a
illyrius666 <FitimQ@live.nl>
2023-12-20 14:42:44 +0000

    -

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,2 +1,7 @@
-pip install --upgrade pip 
+pip install --upgrade pip
 pip install -r requirements.txt
+
+zip_file=".temp/BRXSDK_Bcad_V24_1_05.zip"
+dest_dir="/usr/local/lib/BRXSDK"
+
+sudo unzip $zip_file -d $dest_dir
