5dd705a6556962fb48887f21b6d1128c55f1a31f
illyrius666 <FitimQ@live.nl>
2023-12-21 08:48:38 +0000

    .deploy

--- AutoFrameCAD/cmd/afc.hpp
+++ AutoFrameCAD/cmd/afc.hpp
@@ -1,17 +1,8 @@
 #ifndef AFC_HPP
 #define AFC_HPP
 
-#ifdef _WIN32
-// Windows libraries
 #include "brx_platform_windows.h"
 #include "rxmfcapi.h"
 #include "acutads.h"
-#elif __linux__
-// Linux libraries
-#include "brx_platform_linux.h"
-#elif __APPLE__
-// MacOS libraries
-#include "brx_platform_linux.h"
-#endif
 
 #endif
