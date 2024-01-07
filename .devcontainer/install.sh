243066b7ffaaf2ded2fc9b31fa5f396b24934e6b
illyrius666 <FitimQ@live.nl>
2024-01-07 13:45:07 +0000

    Remove unused CMake extension and add gdb installation

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -17,4 +17,6 @@ function download_and_extract() {
 
 pip install --upgrade pip && pip install -r requirements.txt
 
+sudo apt-get update && sudo apt-get install -y gdb
+
 download_and_extract "https://cloud.structura-engineering.com/cloud-storage/BRXSDK/BRXSDK_Bcad_V24_1_05.zip"
