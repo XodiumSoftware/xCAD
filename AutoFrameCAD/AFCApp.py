10047f199399352b7ef7cdb1ae91bb6ff0870598
illyrius666 <FitimQ@live.nl>
2024-01-12 14:33:37 +0000

    -

--- AutoFrameCAD/AFCApp.py
+++ AutoFrameCAD/AFCApp.py
@@ -1,12 +1,22 @@
 import sys
+from tkinter import Tk as tkTk
 
 from AFCUIModules import PrimaryUIModule as AFCPrimaryUIModule
 
 
-def run() -> None:
-    """Runs the application."""
-    return AFCPrimaryUIModule().mainloop()
+class App:
+    """A class used to represent the application."""
+
+    def __init__(self, modules: list[tkTk]) -> None:
+        """Initializes and runs the application.
+
+        Args:
+            modules (list[tkTk]): The modules to use.
+        """
+        for module in modules:
+            module.mainloop()
+        sys.exit()
 
 
 if __name__ == '__main__':
-    sys.exit(run())
+    App([AFCPrimaryUIModule()])
