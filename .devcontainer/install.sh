491a1ffee0c31732d38ff927e8c8158b6e16eb20
illyrius666 <FitimQ@live.nl>
2024-01-12 09:33:48 +0000

    Update deepsource and devcontainer configurations

--- .devcontainer/install.sh
+++ .devcontainer/install.sh
@@ -1,4 +1,4 @@
-function download_and_extract() {
+function installFromCloud() {
     local source_url=$1
     local file_name=$(basename $source_url)
     local folder_name=$(basename $(dirname $source_url))
@@ -15,10 +15,10 @@ function download_and_extract() {
         sudo unzip -q -o $temp_dir -d $dest_dir
 }
 
-function devcontainer_post_install() {
+function onCreateCommand() {
     pip install --upgrade pip && pip install -r requirements.txt
     sudo apt-get update && sudo apt-get install -y gdb
-    download_and_extract "https://cloud.structura-engineering.com/cloud-storage/BRXSDK/BRXSDK_Bcad_V24_1_05.zip"
+    installFromCloud "https://cloud.structura-engineering.com/cloud-storage/BRXSDK/BRXSDK_Bcad_V24_1_05.zip"
 }
 
-devcontainer_post_install
+onCreateCommand
