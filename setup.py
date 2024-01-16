7561c6ff46821e1c7a911585467d869edd46eaba
illyrius666 <FitimQ@live.nl>
2024-01-16 12:57:55 +0000

    .deploy

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='AutoFrameCAD',
-    version='0.0.73',
+    version='0.0.74',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     license=open('LICENSE.md').read(),
