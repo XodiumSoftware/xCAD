18ce38ba7df2978eae9f65a0e4f0ec405367df43
illyrius666 <FitimQ@live.nl>
2024-03-14 14:24:48 +0000

    Add error handling decorators and refactor json loading function

--- AutoFrameCAD/AFCUtils.py
+++ AutoFrameCAD/AFCUtils.py
@@ -2,11 +2,14 @@ import json
 import re
 from tkinter import filedialog
 
+from AFCDecorators import ErrorHandler as AFCErrorHandler
+
 
 class Utils:
     """A class that contains utility functions."""
 
     @staticmethod
+    @AFCErrorHandler(TypeError)
     def sanitize_str(value: str) -> str:
         """Sanitizes a string.
 
@@ -16,28 +19,28 @@ class Utils:
         return (re.sub(r'\W+', '_', value)).lower()
 
     @staticmethod
+    @AFCErrorHandler(TypeError)
     def get_sql_type(value: int | float | str | bytes | None) -> str:
         """Gets the sql type of a value.
 
         Args:
-            value (int | float | str | bytes): The value to be checked.
+            value (int | float | str | bytes | None): The value to be checked.
         """
-        if value is None:
-            return 'NULL'
         return {
             int: 'INTEGER',
             float: 'REAL',
             str: 'TEXT',
             bytes: 'BLOB',
+            type(None): 'NULL',
         }.get(type(value), 'NULL')
 
     @staticmethod
+    @AFCErrorHandler(FileNotFoundError, IOError)
     def load_json():
         """Loads a json file."""
-        filename = filedialog.askopenfilename(
-            filetypes=[('JSON files', '*.json')]
-        )
-        if filename:
-            with open(filename, 'r') as f:
-                data = json.load(f)
-            return data
+        with open(
+            filedialog.askopenfilename(filetypes=[('JSON files', '*.json')]),
+            'r',
+        ) as f:
+            data = json.load(f)
+        return data
