cf6d678fc2af4484a33dc21a7cf7b0f6d8a94c79
illyrius666 <FitimQ@live.nl>
2024-03-27 13:36:46 +0000

    Update VcXsrv configuration and runArgs in devcontainer.json

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # TODO: Make it in such a way that it gets the newest version from the folder (BRXSDK).
 function installFromCloud() {
     url=$1
@@ -15,7 +17,7 @@ function onCreateCommand() {
     pip install --upgrade pip && pip install -r requirements.txt
     sudo apt-get update && sudo apt-get install -y gdb
     # TODO: Use Onedrive or some sort of api to get it straight from BricsCAD.
-    # installFromCloud "https://cloud.structura-engineering.com/storage/BRXSDK/BRXSDK_Bcad_V24_2_01.zip"
+    # installFromCloud "https://drive.google.com/file/d/1kspi5dyaW4VYLhE0xrPSpEw3eov1CKpD/view?usp=drive_link"
 }
 
 onCreateCommand
