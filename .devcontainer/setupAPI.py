33c1c6278980b399d68c6396e281d3e7a6ebf024
__token__ <FitimQ@live.nl>
2024-03-29 22:18:47 +0000

    -

--- .devcontainer/setupAPI.py
+++ .devcontainer/setupAPI.py
@@ -1,4 +1,8 @@
 import logging
+import os
+import tempfile
+import zipfile
+from urllib.parse import urlparse
 
 import requests
 
@@ -7,7 +11,11 @@ class SetupAPI:
     """A class to setup the API for the Bricsys-API repository."""
 
     def __init__(
-        self, org_name: str, repo_name: str, access_token: str
+        self,
+        org_name: str,
+        repo_name: str,
+        access_token: str,
+        api_path: str,
     ) -> None:
         """Initialize the class.
 
@@ -15,50 +23,132 @@ class SetupAPI:
             org_name (str): The name of the organization.
             repo_name (str): The name of the repository.
             access_token (str): The access token.
+            api_path (str): The path to the API.
+                Use [BRX/TX] to get a list of all the files in the folder.
         """
-        self.session = requests.Session()
-        self.session.headers.update(
+        self.logger = self._logger()
+        self._processor(
+            self._fetcher(
+                f'https://api.github.com/repos/{org_name}/{repo_name}/contents',
+                self._session(access_token),
+                api_path,
+            ),
+        )
+
+    @staticmethod
+    def _logger() -> logging.Logger:
+        """Setup the logger."""
+        logging.basicConfig(level=logging.INFO)
+        return logging.getLogger(__name__)
+
+    @staticmethod
+    def _session(access_token: str) -> requests.Session:
+        """Setup the session with headers.
+
+        Args:
+            access_token (str): The access token.
+        """
+        session = requests.Session()
+        session.headers.update(
             {
                 'Authorization': f'token {access_token}',
                 'Accept': 'application/vnd.github.v3+json',
             }
         )
+        return session
 
-        self.base_url = (
-            f'https://api.github.com/repos/{org_name}/{repo_name}/contents'
-        )
-
-        logging.basicConfig(level=logging.INFO)
-        self.logger = logging.getLogger(__name__)
-
-    def get_contents(self, path: str = '') -> None:
-        """Get the contents of the repository.
+    def _fetcher(
+        self,
+        base_url: str,
+        session: requests.Session,
+        path: str = '',
+    ) -> dict[str, str] | list[dict[str, str]]:
+        """Fetch the contents of the repository.
 
         Args:
-            path (str, optional): The path to the repository. Defaults to ''.
+            base_url (str): The base URL.
+            session (requests.Session): The session.
+            path (str, optional): The path to the contents. Defaults to ''.
         """
-        url = f'{self.base_url}/{path}'
         try:
-            response = self.session.get(url)
+            response = session.get(f'{base_url}/{path}')
             response.raise_for_status()
-        except requests.exceptions.RequestException as e:
+        except requests.RequestException as e:
             self.logger.error(f'Request failed: {e}')
             raise
+        return response.json()
 
-        data = response.json()
+    def _processor(
+        self,
+        fetcher: dict[str, str] | list[dict[str, str]],
+    ) -> None:
+        """Process the contents of the repository.
 
-        for file in data:
-            if file['type'] == 'dir':
-                self.get_contents(file['path'])
-            else:
-                self.logger.info(file['download_url'])
+        Args:
+            fetcher (dict[str, str] | list[dict[str, str]]):
+                The fetcher.
+        """
+        if isinstance(fetcher, dict):
+            if fetcher.get('download_url'):
+                self.logger.info(fetcher['download_url'])
+                temp_path = self._downloader(fetcher['download_url'])
+                self._installer(temp_path)
+        else:
+            for file in fetcher:
+                if file.get('download_url'):
+                    self.logger.info(file['download_url'])
+                    temp_path = self._downloader(file['download_url'])
+                    self._installer(temp_path)
 
-        if 'next' in response.links:
-            self.get_contents(response.links['next']['url'])
+    def _downloader(self, url: str) -> str:
+        """Download the API.
+
+        Args:
+            url (str): The URL of the API.
+        """
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
+
+        temp_path = os.path.join(
+            tempfile.gettempdir(), os.path.basename(urlparse(url).path)
+        )
+
+        with open(temp_path, 'wb') as file:
+            for chunk in response.iter_content(chunk_size=8192):
+                file.write(chunk)
+
+        self.logger.info(f'Downloaded to: {temp_path}')
+        return temp_path
+
+    def _installer(
+        self, temp_path: str, install_path: str = os.path.expanduser('~/lib')
+    ) -> None:
+        """Install the API.
+
+        Args:
+            temp_path (str): The temporary path.
+            install_path (str, optional): The install path.
+                Defaults to os.path.expanduser('~/lib').
+        """
+        new_dir_path = os.path.join(
+            install_path, os.path.splitext(os.path.basename(temp_path))[0]
+        )
+
+        os.makedirs(new_dir_path, exist_ok=True)
+
+        with zipfile.ZipFile(temp_path, 'r') as zip_ref:
+            zip_ref.extractall(new_dir_path)
+
+        self.logger.info(f'Unzipped files to: {new_dir_path}')
+
+        os.remove(temp_path)
+
+        self.logger.info(f'Removed the zip file: {temp_path}')
 
 
 SetupAPI(
-    'Structura-Engineering',
-    'Bricsys-API',
-    '',
-).get_contents()
+    org_name='Structura-Engineering',
+    repo_name='Bricsys-API',
+    access_token='',
+    api_path='BRX/BRXSDK_Bcad_V24_2_03-1.zip',
+)
