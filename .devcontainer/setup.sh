81d6959dfb32eafc51baa1e523c7adab2433aeec
illyrius666 <FitimQ@live.nl>
2024-03-29 13:07:35 +0000

    Update setup.sh and c_cpp_properties.json

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-# TODO: Make it in such a way that it gets the newest version from the folder (BRXSDK).
-function installFromCloud() {
-    url=$1
-    filename=$(basename $url)
-    foldername=${filename%.*}
-    sudo wget $url
-    sudo mkdir -p /usr/local/lib/$foldername
-    sudo unzip -d /usr/local/lib/$foldername $filename
-    sudo rm $filename
+function installLibrary() {
+    repo=$1
+    api=$2
+    git clone $repo temp_repo
+    cd temp_repo/workspaces/Bricsys-API/$api
+    latest_zip=$(ls -Art | grep .zip | tail -n 1)
+    unzip $latest_zip -d /usr/local/bin
     echo 'export PATH=$PATH:/usr/local/lib/' >> ~/.bashrc
     source ~/.bashrc
+    cd ../../..
+    rm -rf temp_repo
 }
 
 function onCreateCommand() {
     pip install --upgrade pip && pip install -r requirements.txt
     sudo apt-get update && sudo apt-get install -y gdb
-    # TODO: Use Onedrive or some sort of api to get it straight from BricsCAD.
-    # installFromCloud "https://drive.google.com/file/d/1kspi5dyaW4VYLhE0xrPSpEw3eov1CKpD/view?usp=drive_link"
+    installLibrary "https://github.com/Structura-Engineering/Bricsys-API.git" "BRX" # or "TX"
 }
 
 onCreateCommand
