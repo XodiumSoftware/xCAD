55761d31e2684a2a04826c85da6fbe902603b148
illyrius666 <fitimq@live.nl>
2024-04-22 08:51:51 +0000

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import find_packages, setup
 setup(
     name="AutoFrameCAD",
     version="0.0.81",
-    url="https://github.com/Structura-Engineering/AutoFrameCad",
+    url="https://github.com/Structura-Engineering/AutoFrameCAD",
     license=open("LICENSE.md").read(),
+    description="CAD addon/plug-in to automize frameworks.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Structura Engineering",
@@ -13,9 +14,24 @@ setup(
     install_requires=open("requirements.txt").read().splitlines(),
     python_requires=">=3.12",
     package_data={"*": ["*.py", "py.typed"]},
-    data_files=[("", ["LICENSE.md", "README.md", "requirements.txt"])],
+    data_files=[
+        (
+            "",
+            [
+                "CHANGELOG.md",
+                "CODE_OF_CONDUCT.md",
+                "CONTRIBUTING.md",
+                "LICENSE.md",
+                "README.md",
+                "requirements.txt",
+            ],
+        )
+    ],
     zip_safe=False,
     project_urls={
-        "Bug Tracker": "https://github.com/Structura-Engineering/AutoFrameCad/issues"
+        "Source Code": "https://github.com/Structura-Engineering/AutoFrameCAD",
+        "Bug Tracker": "https://github.com/Structura-Engineering/AutoFrameCAD/issues",
+        "Documentation": "https://wiki.structura-engineering.com/AutoFrameCAD",
+        "Funding": "https://buymeacoffee.com/illyrius",
     },
 )
