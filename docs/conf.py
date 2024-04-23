5dbc12d0f99c972d1a71d21773fa5a3129799881
illyrius666 <fitimq@live.nl>
2024-04-23 17:34:51 +0000

    .deploy

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,6 @@ release = "1.0"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.napoleon",
-    "sphinx_reredirects",
 ]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
@@ -70,9 +69,3 @@ napoleon_use_rtype = True
 napoleon_preprocess_types = False
 napoleon_type_aliases = None
 napoleon_attr_annotations = True
-
-# -- Options for sphinx_reredirects -------------------------------------------
-# https://documatt.com/sphinx-reredirects/usage.html
-redirects = {
-    "_modules/*": "*",
-}
