12860b4855eafe142d8af38c4b689a3332e8d483
illyrius666 <FitimQ@live.nl>
2024-04-24 08:55:41 +0000

    .deploy

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,6 @@ sys.path.insert(0, os.path.abspath("../"))
 
 
 # Configuration file for the Sphinx documentation builder.
-#
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
@@ -45,6 +44,8 @@ html_theme_options = {
 html_title = "STEN Wiki"
 html_logo = "https://raw.githubusercontent.com/Structura-Engineering/.github/main/icons/favicon.webp"
 html_favicon = "https://raw.githubusercontent.com/Structura-Engineering/.github/main/icons/sten_short_logo.svg"
+html_static_path = ["_static"]
+html_css_files = ["custom.css"]
 
 # -- Options for autodoc -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/extensions/autodoc.html
@@ -54,9 +55,9 @@ html_favicon = "https://raw.githubusercontent.com/Structura-Engineering/.github/
 # https://www.sphinx-doc.org/en/master/usage/extensions/napoleon.html
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
-napoleon_include_init_with_doc = True
-napoleon_include_private_with_doc = True
-napoleon_include_special_with_doc = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = False
 napoleon_use_admonition_for_examples = False
 napoleon_use_admonition_for_notes = False
 napoleon_use_admonition_for_references = False
