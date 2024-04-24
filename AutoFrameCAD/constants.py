bf0d4f210bee79e8901f80342158b0f87f51dc98
illyrius666 <FitimQ@live.nl>
2024-04-24 11:59:47 +0000

    -

--- AutoFrameCAD/constants.py
+++ AutoFrameCAD/constants.py
@@ -5,23 +5,22 @@ from StenLib import Utils
 # General
 # =============================================================================
 BASE_DIR = Path(__file__).resolve().parent
-UTILS = Utils
 # =============================================================================
 
 # Icons
 # =============================================================================
-ICON_DIR = UTILS.forge.dir(BASE_DIR / "icons")
+ICON_DIR = Utils.forge.dir(BASE_DIR / "icons")
 UI_ICON_FILE = ICON_DIR / "ui.svg"
 # =============================================================================
 
 # .so Lib files
 # =============================================================================
-LIB_DIR = UTILS.forge.dir(BASE_DIR / "build")
+LIB_DIR = Utils.forge.dir(BASE_DIR / "build")
 UTILS_LIB_FILE = LIB_DIR / "test.so"
 # =============================================================================
 
 # Database
 # =============================================================================
-DATABASE_DIR = UTILS.forge.dir(BASE_DIR / "database")
-DATABASE_FILE = UTILS.forge.file(DATABASE_DIR / "autoframecad.sqlite")
+DATABASE_DIR = Utils.forge.dir(BASE_DIR / "database")
+DATABASE_FILE = Utils.forge.file(DATABASE_DIR / "autoframecad.sqlite")
 # =============================================================================
