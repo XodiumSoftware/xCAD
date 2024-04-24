9a0f4bfe26c550ffcaa931c1e40aec973a9081c9
illyrius666 <FitimQ@live.nl>
2024-04-24 09:24:44 +0000

    .deploy

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,7 @@
 import os
+import pkgutil
 import sys
+from pathlib import Path
 
 sys.path.insert(0, os.path.abspath("../"))
 
@@ -67,3 +69,57 @@ napoleon_use_rtype = True
 napoleon_preprocess_types = False
 napoleon_type_aliases = None
 napoleon_attr_annotations = True
+
+# -- Generate _modules files --------------------------------------------------
+
+
+class RSTGenerator:
+    """A class to generate rst files for the modules."""
+
+    def __init__(self, module_names: list[str], output_dir: str) -> None:
+        """Initialize the RSTGenerator.
+
+        Args:
+            module_names (list[str]): The names of the modules.
+            output_dir (str): The output directory.
+        """
+        self.module_names = module_names
+        self.output_dir = Path(output_dir)
+
+    @staticmethod
+    def generate_rst(module_name: str) -> str:
+        """Generate the rst content for a module.
+
+        Args:
+            module_name (str): The name of the module.
+
+        Returns:
+            str: The rst content for the module.
+        """
+        rst_content = f"{module_name}\n"
+        rst_content += "=" * len(module_name) + "\n\n"
+
+        for _, name, _ in pkgutil.iter_modules(
+            [os.path.join("..", module_name)]
+        ):
+            if name == "tests":
+                continue
+            rst_content += f".. automodule:: {module_name}.{name}\n"
+            rst_content += "   :members:\n"
+            rst_content += "   :undoc-members:\n"
+            rst_content += "   :show-inheritance:\n\n"
+
+        return rst_content
+
+    def write_rst_files(self) -> None:
+        """Write the rst files for the modules."""
+        os.makedirs(self.output_dir, exist_ok=True)
+        for module_name in self.module_names:
+            rst_content = self.generate_rst(module_name)
+            with open(
+                os.path.join(self.output_dir, f"{module_name}.rst"), "wb"
+            ) as f:
+                f.write(rst_content.encode())
+
+
+RSTGenerator(["AutoFrameCAD", "StenLib"], "_modules").write_rst_files()
