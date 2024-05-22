593966fdc5b07937320ac4f6ab71b4f1d9303d14
illyrius666 <FitimQ@live.nl>
2024-05-22 09:24:27 +0000

    -

--- autoframecad/basic_pline.cpp
+++ autoframecad/basic_pline.cpp
@@ -1,40 +1,30 @@
-#include <aced.h>
-#include <dbents.h>
-#include <dbapserv.h>
-
-void DrawLine()
-{
-    AcGePoint3d startPt(0.0, 0.0, 0.0);
-    AcGePoint3d endPt(10.0, 10.0, 0.0);
-
-    AcDbLine *pLine = new AcDbLine(startPt, endPt);
-    AcDbBlockTable *pBlockTable;
-    acdbHostApplicationServices()->workingDatabase()->getBlockTable(pBlockTable, AcDb::kForRead);
-
-    AcDbBlockTableRecord *pBlockTableRecord;
-    pBlockTable->getAt(ACDB_MODEL_SPACE, pBlockTableRecord, AcDb::kForWrite);
-    pBlockTable->close();
-
-    pBlockTableRecord->appendAcDbEntity(pLine);
-    pBlockTableRecord->close();
-
-    acutPrintf("Line drawn successfully.\n");
-}
-
-void HelloWorldCmd()
-{
-    DrawLine();
-}
-
-extern "C" __declspec(dllexport) AcRx::AppRetCode acrxEntryPoint(AcRx::AppMsgCode msg, void *pkt)
+#include "rxregsvc.h"
+#include "acutads.h"
+// Simple acrxEntryPoint code. Normally intialization and cleanup
+// (such as registering and removing commands) should be done here.
+//
+extern "C" AcRx::AppRetCode
+acrxEntryPoint(AcRx::AppMsgCode msg, void *appId)
 {
     switch (msg)
     {
     case AcRx::kInitAppMsg:
-        acedRegCmds->addCommand(_T("HELLO"), _T("HELLO"), _T("WORLD"), ACRX_CMD_MODAL, HelloWorldCmd);
+        // Allow application to be unloaded
+        // Without this statement, AutoCAD will
+        // not allow the application to be unloaded
+        // except on AutoCAD exit.
+        //
+        acrxUnlockApplication(appId);
+        // Register application as MDI aware.
+        // Without this statement, AutoCAD will
+        // switch to SDI mode when loading the
+        // application.
+        //
+        acrxRegisterAppMDIAware(appId);
+        acutPrintf("\nAutoFrameCAD Loaded");
         break;
     case AcRx::kUnloadAppMsg:
-        acedRegCmds->removeGroup(_T("HELLO"));
+        acutPrintf("\nAutoFrameCAD Unloaded");
         break;
     }
     return AcRx::kRetOK;
