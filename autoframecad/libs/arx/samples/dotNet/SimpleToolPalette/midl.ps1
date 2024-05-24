181f1bcb33f5e64bde9a4caaf6e72dc5c963efd9
illyrius666 <FitimQ@live.nl>
2024-05-24 14:06:32 +0000

    the arx lib is only windows and macos based.

--- autoframecad/libs/arx/samples/dotNet/SimpleToolPalette/midl.ps1
+++
@@ -1,5 +0,0 @@
-$vsInstallPath = & "${env:ProgramFiles(x86)}/Microsoft Visual Studio/Installer/vswhere.exe" -latest -property installationPath;
-Import-Module "$vsInstallPath/Common7/Tools/Microsoft.VisualStudio.DevShell.dll"
-Enter-VsDevShell -VsInstallPath $vsInstallPath -SkipAutomaticLocation
-
-& midl.exe /header nul /iid nul /proxy nul /dlldata nul $args[0]
