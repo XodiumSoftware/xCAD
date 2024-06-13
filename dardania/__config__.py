172ea0916448f03ea4a201818b9e1544665cd805
illyrius666 <FitimQ@live.nl>
2024-06-13 11:35:48 +0000

    chore: Update devcontainer.json, optimize images, and update CI/CD workflow

--- dardania/__config__.py
+++ dardania/__config__.py
@@ -12,7 +12,7 @@ BASE_DIR: Path = Path(__file__).resolve().parent
 # Icons
 # =============================================================================
 ICON_DIR: Path = Utils.forge.dir(BASE_DIR / "icons")
-UI_ICON_FILE: Path = ICON_DIR / "ui.png"
+UI_ICON_FILE: Path = ICON_DIR / "illyrion.svg"
 DARK_MODE_ICON_FILE: Path = ICON_DIR / "dark_mode.png"
 LIGHT_MODE_ICON_FILE: Path = ICON_DIR / "light_mode.png"
 # =============================================================================
