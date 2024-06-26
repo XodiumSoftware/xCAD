ffde68293587db7e7304d008dd4689b70b34bc4c
illyrius666 <FitimQ@live.nl>
2024-06-26 12:48:29 +0000

    chore: moved some values to the __config__ refactored the widget grid added a OpenQL view added a splitter for the body made the tree have equal header widths and made the columns in the tree sortable

--- dardania/__config__.py
+++ dardania/__config__.py
@@ -12,9 +12,8 @@ BASE_DIR: Path = Path(__file__).resolve().parent
 # Icons
 # =============================================================================
 ICON_DIR: Path = Utils.forge.dir(BASE_DIR / "icons")
-UI_ICON_FILE: Path = ICON_DIR / "illyrion.svg"
-DARK_MODE_ICON_FILE: Path = ICON_DIR / "dark_mode_icon.svg"
-LIGHT_MODE_ICON_FILE: Path = ICON_DIR / "light_mode_icon.svg"
+DARK_MODE_ICON: Path = ICON_DIR / "dark_mode_icon.svg"
+LIGHT_MODE_ICON: Path = ICON_DIR / "light_mode_icon.svg"
 # =============================================================================
 
 # .so Lib files
@@ -34,8 +33,15 @@ PREFERENCES_DATA: dict[str, None | int | float | str | bytes] = {
     USR_THEME: DARK_MODE,
 }
 ICON_THEMES: dict[str, str] = {
-    DARK_MODE: str(DARK_MODE_ICON_FILE),
-    LIGHT_MODE: str(LIGHT_MODE_ICON_FILE),
+    DARK_MODE: str(DARK_MODE_ICON),
+    LIGHT_MODE: str(LIGHT_MODE_ICON),
 }
 THEMES: dict[str, str] = {DARK_MODE: LIGHT_MODE, LIGHT_MODE: DARK_MODE}
+# =============================================================================
+
+# UI
+# =============================================================================
+WINDOW_TITLE: str = "Dardania"
+WINDOW_ICON: Path = ICON_DIR / "illyrion.svg"
+WINDOW_MIN_SIZE: tuple[int, int] = 1200, 800
 # =============================================================================
--- dardania/ui.py
+++ dardania/ui.py
@@ -2,24 +2,27 @@
 
 from __config__ import (
     DATABASE_FILE,
-    UI_ICON_FILE,
+    WINDOW_ICON,
+    WINDOW_MIN_SIZE,
+    WINDOW_TITLE,
 )
 from dalmatia import Utils
 from db_tables import PreferencesTable
 from handlers.theme import Theme
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QFont, QIcon
+from PySide6.QtOpenGLWidgets import QOpenGLWidget
 from PySide6.QtWidgets import (
     QGridLayout,
+    QHeaderView,
     QLabel,
     QMainWindow,
     QPushButton,
+    QSizePolicy,
+    QSplitter,
     QTreeWidget,
     QWidget,
 )
-
-WINDOW_TITLE: str = "Dardania"
-WINDOW_MIN_SIZE: tuple[int, int] = 1200, 800
 
 
 class UI(QMainWindow):
@@ -33,7 +36,7 @@ class UI(QMainWindow):
         self.handle_theme = Theme(self.db, self.preferences_table)
 
         self.setWindowTitle(WINDOW_TITLE)
-        self.setWindowIcon(QIcon(str(UI_ICON_FILE)))
+        self.setWindowIcon(QIcon(str(WINDOW_ICON)))
         self.setMinimumSize(*WINDOW_MIN_SIZE)
 
         self.setCentralWidget(QWidget(self))
@@ -43,21 +46,13 @@ class UI(QMainWindow):
         self._body()
         self._footer()
 
-        list(
-            map(
-                self.__layout__.addWidget,
-                [
-                    self.header_title,
-                    self.body_props_tree,
-                    self.footer_copyright,
-                    self.footer_theme_button,
-                ],
-                [0, 1, 2, 2],
-                [0, 0, 0, 1],
-                [1, 1, 1, 1],
-                [2, 2, 1, 1],
-            ),
-        )
+        for widget, params in {
+            self.header_title: [0, 0, 1, 2],
+            self.body_splitter: [1, 0, 1, 2],
+            self.footer_copyright: [2, 0, 1, 1],
+            self.footer_theme_button: [2, 1, 1, 1],
+        }.items():
+            self.__layout__.addWidget(widget, *params)
 
         self.handle_theme.set_theme(self)
 
@@ -70,12 +65,29 @@ class UI(QMainWindow):
     def _body(self: "UI") -> None:
         """Create the body."""
         self.body_props_tree = QTreeWidget()
-        self.body_props_tree.setColumnCount(2)
-        self.body_props_tree.setHeaderLabels(["Property", "Value"])
+        props_tree_headers: list[str] = ["Property", "Value"]
+        self.body_props_tree.setColumnCount(len(props_tree_headers))
+        self.body_props_tree.setHeaderLabels(props_tree_headers)
+        self.body_props_tree.setSortingEnabled(True)
+        self.body_props_tree.sortItems(0, Qt.SortOrder.AscendingOrder)
+        self.body_props_tree.header().setSectionResizeMode(
+            QHeaderView.ResizeMode.Stretch,
+        )
+
+        self.body_viewer = QOpenGLWidget()
+
+        self.body_splitter = QSplitter(Qt.Orientation.Horizontal)
+        self.body_splitter.setSizePolicy(
+            QSizePolicy.Policy.Preferred,
+            QSizePolicy.Policy.Expanding,
+        )
+        for widget in (self.body_props_tree, self.body_viewer):
+            self.body_splitter.addWidget(widget)
 
     def _footer(self: "UI") -> None:
         """Create the footer."""
         self.footer_copyright = QLabel("©2024 Illyrion")
+
         self.footer_theme_button = QPushButton()
         self.footer_theme_button.setMaximumWidth(40)
         self.handle_theme.set_theme_icon(self.footer_theme_button)
