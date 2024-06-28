7a67d6df16b95a4f9e9a35bc5e52fa01c2a43686
illyrius666 <FitimQ@live.nl>
2024-06-28 08:27:51 +0000

    chore: Update pip installation command in devcontainer.json
    The pip installation command in devcontainer.json has been updated to include upgrading pip to the latest version. This ensures that the dependencies are installed with the latest version of pip.
    
    Also, update the package version in setup.py
    
    The package version in setup.py has been updated to "v1.0.0" to align with the project's versioning convention.
    
    Update requirements.txt
    
    The requirement "dalmatia" has been changed to "dalmatium" in requirements.txt.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="Dardanium",
-    version="1.0.0",
+    version="v1.0.0",
     url="https://github.com/IllyrionSoftware/Dardanium",
     license=Path("LICENSE.md").read_text(),
     description="CAD addon/plug-in to automize frameworks.",
@@ -18,18 +18,6 @@ setup(
     install_requires=Path("requirements.txt").read_text().splitlines(),
     python_requires=">=3.12",
     package_data={"*": ["*.py", "py.typed"]},
-    data_files=[
-        (
-            "",
-            [
-                "CODE_OF_CONDUCT.md",
-                "CONTRIBUTING.md",
-                "LICENSE.md",
-                "README.md",
-                "requirements.txt",
-            ],
-        ),
-    ],
     zip_safe=False,
     project_urls={
         "Source Code": "https://github.com/IllyrionSoftware/Dardanium",
