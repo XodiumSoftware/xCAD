51c3b986fce1b2ef9a0900e4c720ac47062d9619
illyrius666 <fitimq@live.nl>
2024-07-01 14:53:55 +0000

    chore: Refactor acrxEntryPoint.cpp for improved readability and maintainability

--- dardanium/acrxEntryPoint.cpp
+++ dardanium/acrxEntryPoint.cpp
@@ -3,12 +3,27 @@
 // Simple acrxEntryPoint code. Normally intialization and cleanup
 // (such as registering and removing commands) should be done here.
 //
+
+// Define constants for application messages
+const AcRx::AppMsgCode INIT_APP_MSG = AcRx::kInitAppMsg;
+const AcRx::AppMsgCode UNLOAD_APP_MSG = AcRx::kUnloadAppMsg;
+
+// Define constants for return values
+const AcRx::AppRetCode SUCCESS_RET_CODE = AcRx::kRetOK;
+
+// Define prefix constant
+#define PREFIX L"Dardanium"
+
+// Define constants for strings
+const wchar_t *LOADED_MSG = PREFIX L" Loaded";
+const wchar_t *UNLOADED_MSG = PREFIX L" Unloaded";
+
 extern "C" AcRx::AppRetCode
 acrxEntryPoint(AcRx::AppMsgCode msg, void *appId)
 {
     switch (msg)
     {
-    case AcRx::kInitAppMsg:
+    case INIT_APP_MSG:
         // Allow application to be unloaded
         // Without this statement, AutoCAD will
         // not allow the application to be unloaded
@@ -21,11 +36,11 @@ acrxEntryPoint(AcRx::AppMsgCode msg, void *appId)
         // application.
         //
         acrxRegisterAppMDIAware(appId);
-        acutPrintf(ACRX_T("\nDardania Loaded"));
+        acutPrintf(LOADED_MSG);
         break;
-    case AcRx::kUnloadAppMsg:
-        acutPrintf(ACRX_T("\nDardania Unloaded"));
+    case UNLOAD_APP_MSG:
+        acutPrintf(UNLOADED_MSG);
         break;
     }
-    return AcRx::kRetOK;
+    return SUCCESS_RET_CODE;
 }
