775749626f07640cada5e3cf9a530c1576783fae
illyrius666 <FitimQ@live.nl>
2024-09-05 10:21:06 +0000

    - removed slint framework + added egui framework

--- xcad/src/main.rs
+++ xcad/src/main.rs
@@ -1,18 +1,27 @@
-// Prevent console window in addition to Slint window in Windows release builds when, e.g., starting the app via file manager. Ignored on other platforms.
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![warn(clippy::all, rust_2018_idioms)]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-slint::include_modules!();
+// When compiling natively:
+#[cfg(not(target_arch = "wasm32"))]
+fn main() -> eframe::Result {
+    env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
-fn main() -> Result<(), slint::PlatformError> {
-    let ui: AppWindow = AppWindow::new()?;
-
-    ui.on_request_increase_value({
-        let ui_handle: slint::Weak<AppWindow> = ui.as_weak();
-        move || {
-            let ui: AppWindow = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });
-
-    ui.run()
+    let native_options: eframe::NativeOptions = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default()
+            .with_inner_size([400.0, 300.0])
+            .with_min_inner_size([300.0, 220.0])
+            .with_icon(
+                // NOTE: Adding an icon is optional
+                eframe::icon_data::from_png_bytes(
+                    &include_bytes!("../assets/icons/icon-256.png")[..],
+                )
+                .expect("Failed to load icon"),
+            ),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "eframe template",
+        native_options,
+        Box::new(|cc: &eframe::CreationContext<'_>| Ok(Box::new(xcad::TemplateApp::new(cc)))),
+    )
 }
