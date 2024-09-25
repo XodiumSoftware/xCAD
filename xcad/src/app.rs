8eb0020dd7d203490615f1175ad4b74754c23200
Illyrius <28700752+illyrius666@users.noreply.github.com>
2024-09-25 12:21:41 +0200

    v0.2.1 (#247)
    Signed-off-by: Illyrius <28700752+illyrius666@users.noreply.github.com>

--- xcad/src/app.rs
+++ xcad/src/app.rs
@@ -5,6 +5,36 @@ const IS_TARGET_WASM32: bool = cfg!(target_arch = "wasm32");
 const MENU_FILE_TITLE: &str = "File";
 const MENU_QUIT_TITLE: &str = "🚪 Quit";
 
+// Body
+const TABLE_DATA: &[(&str, &[(&str, &str)])] = &[
+    (
+        "General",
+        &[
+            ("Color", "Yellow"),
+            ("Layer", "AL32 ----_ BINNENKOZIJN"),
+            ("Linetype", "ByLayer"),
+            ("Linetype scale", "1"),
+            ("Lineweight", "ByLayer"),
+            ("Transparency", "ByLayer"),
+        ],
+    ),
+    (
+        "View",
+        &[
+            ("Elevation", "0 mm"),
+            ("Perspective", "Off"),
+            ("Lens length", "50.000 mm"),
+            ("Field of view", "38.58"),
+            ("Height", "43955.762 mm"),
+            ("Width", "64214.102 mm"),
+        ],
+    ),
+    (
+        "Misc",
+        &[("Annotation scale", "1:1"), ("Default lighting", "On")],
+    ),
+];
+
 // Footer
 const COPYRIGHT: &str = "© 2024 XODIUM SOFTWARE INC";
 const EFRAME_URL: &str = "https://github.com/emilk/egui/tree/master/crates/eframe";
@@ -30,6 +60,7 @@ impl eframe::App for App {
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
+
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         self.header(ctx);
         self.footer(ctx);
@@ -44,6 +75,7 @@ impl App {
         }
         Default::default()
     }
+
     fn header(&mut self, ctx: &egui::Context) {
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
             egui::menu::bar(ui, |ui| {
@@ -60,68 +92,45 @@ impl App {
             });
         });
     }
+
     fn body(&mut self, ctx: &egui::Context) {
-        let table_data = vec![
-            (
-                "General",
-                vec![
-                    ("Color", "Yellow"),
-                    ("Layer", "AL32 ----_ BINNENKOZIJN"),
-                    ("Linetype", "ByLayer"),
-                    ("Linetype scale", "1"),
-                    ("Lineweight", "ByLayer"),
-                    ("Transparency", "ByLayer"),
-                ],
-            ),
-            (
-                "View",
-                vec![
-                    ("Elevation", "0 mm"),
-                    ("Perspective", "Off"),
-                    ("Lens length", "50.000 mm"),
-                    ("Field of view", "38.58"),
-                    ("Height", "43955.762 mm"),
-                    ("Width", "64214.102 mm"),
-                ],
-            ),
-            (
-                "Misc",
-                vec![("Annotation scale", "1:1"), ("Default lighting", "On")],
-            ),
-        ];
         let text_height = egui::TextStyle::Body
             .resolve(&ctx.style())
             .size
             .max(ctx.style().spacing.interact_size.y);
+
         egui::CentralPanel::default().show(ctx, |ui| {
-            for (category, properties) in &table_data {
-                egui::CollapsingHeader::new(egui::RichText::new(*category).strong())
-                    .default_open(true)
-                    .show(ui, |ui| {
-                        egui_extras::TableBuilder::new(ui)
-                            .striped(true)
-                            .resizable(true)
-                            .column(
-                                egui_extras::Column::initial(self.init_col_x)
-                                    .at_least(self.min_col_x),
-                            )
-                            .column(egui_extras::Column::remainder().at_least(self.min_col_x))
-                            .body(|mut body| {
-                                for (property, value) in properties {
-                                    body.row(text_height, |mut row| {
-                                        row.col(|ui| {
-                                            ui.label(*property);
+            egui::ScrollArea::vertical().show(ui, |ui| {
+                for (category, properties) in TABLE_DATA.iter() {
+                    egui::CollapsingHeader::new(egui::RichText::new(*category).strong())
+                        .default_open(true)
+                        .show(ui, |ui| {
+                            egui_extras::TableBuilder::new(ui)
+                                .striped(true)
+                                .resizable(true)
+                                .column(
+                                    egui_extras::Column::initial(self.init_col_x)
+                                        .at_least(self.min_col_x),
+                                )
+                                .column(egui_extras::Column::remainder().at_least(self.min_col_x))
+                                .body(|mut body| {
+                                    for (property, value) in properties.iter() {
+                                        body.row(text_height, |mut row| {
+                                            row.col(|ui| {
+                                                ui.label(*property);
+                                            });
+                                            row.col(|ui| {
+                                                ui.label(*value);
+                                            });
                                         });
-                                        row.col(|ui| {
-                                            ui.label(*value);
-                                        });
-                                    });
-                                }
-                            });
-                    });
-            }
+                                    }
+                                });
+                        });
+                }
+            });
         });
     }
+
     fn footer(&mut self, ctx: &egui::Context) {
         egui::TopBottomPanel::bottom("bottom_panel").show(ctx, |ui| {
             ui.horizontal(|ui| {
