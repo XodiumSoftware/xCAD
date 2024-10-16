a62f49a24110ced6abde2a34703da5174f93bc00
illyrius666 <fitimq@live.nl>
2024-10-16 10:00:52 +0000

    +

--- xcad/src/db.rs
+++ xcad/src/db.rs
@@ -1,12 +1,10 @@
 use crate::bim::BIMObject;
 use sqlx::sqlite::SqlitePoolOptions;
 use sqlx::{Pool, Result, Sqlite};
-use tokio::sync::OnceCell;
 
 #[derive(Clone)]
 pub struct DBManager {
     pool: Pool<Sqlite>,
-    table_initialized: OnceCell<()>,
 }
 
 impl DBManager {
@@ -16,29 +14,10 @@ impl DBManager {
                 .max_connections(5)
                 .connect(db_url)
                 .await?,
-            table_initialized: OnceCell::new(),
         })
     }
 
-    async fn ensure_table(&self) -> Result<()> {
-        self.table_initialized
-            .get_or_init(|| async {
-                sqlx::query!(
-                    "CREATE TABLE IF NOT EXISTS bim_objects (
-                        id TEXT PRIMARY KEY,
-                        name TEXT NOT NULL,
-                        desc TEXT
-                    )"
-                )
-                .execute(&self.pool)
-                .await
-            })
-            .await
-            .clone()
-    }
-
     pub async fn set_obj(&self, bim: &BIMObject) -> Result<u64> {
-        self.ensure_table().await?;
         sqlx::query!(
             "INSERT INTO bim_objects (id, name, desc) VALUES ($1, $2, $3)
             ON CONFLICT (id) DO UPDATE SET name = EXCLUDED.name, desc = EXCLUDED.desc",
