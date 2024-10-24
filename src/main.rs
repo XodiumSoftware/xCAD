379634896ace508d5687f581bc6cd331730fc4c8
illyrius666 <fitimq@live.nl>
2024-10-24 13:09:24 +0000

    + restructering

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use server::Server;
 
 // #[actix_web::main]
 // async fn main() -> Result<()> {
-//     let db = Database::new("xcad/data/xcad.db", "TEST_TABLE")?;
+//     let db = Database::new("src/data/xcad.db", "TEST_TABLE")?;
 //     let bind_address = "127.0.0.1:8080";
 //     Server::new(db, bind_address)
 //         .run()
