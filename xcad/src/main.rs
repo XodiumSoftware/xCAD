234b81d496e850367786fac482efe228a7a01c38
illyrius666 <fitimq@live.nl>
2024-10-24 12:54:24 +0000

    +

--- xcad/src/main.rs
+++ xcad/src/main.rs
@@ -3,25 +3,63 @@ mod auth;
 mod db;
 mod server;
 
-use app::App;
 use db::Database;
 use rusqlite::Result;
 use server::Server;
 
-#[actix_web::main]
-async fn main() -> Result<()> {
-    let db = Database::new("xcad/data/xcad.db", "TEST_TABLE")?;
-    let bind_address = "127.0.0.1:8080";
-    Server::new(db, bind_address)
-        .run()
-        .await
-        .map_err(|e| rusqlite::Error::ToSqlConversionFailure(Box::new(e)))?;
-    if let Err(e) = eframe::run_native(
-        "xCAD Frontend",
-        eframe::NativeOptions::default(),
-        Box::new(|_cc| Ok(Box::new(App::default()) as Box<dyn eframe::App>)),
-    ) {
-        eprintln!("Error running eframe: {}", e);
-    }
-    Ok(())
+// #[actix_web::main]
+// async fn main() -> Result<()> {
+//     let db = Database::new("xcad/data/xcad.db", "TEST_TABLE")?;
+//     let bind_address = "127.0.0.1:8080";
+//     Server::new(db, bind_address)
+//         .run()
+//         .await
+//         .map_err(|e| rusqlite::Error::ToSqlConversionFailure(Box::new(e)))?;
+//     Ok(())
+// }
+
+// #[cfg(target_arch = "wasm32")]
+fn main() {
+    use eframe::wasm_bindgen::JsCast as _;
+
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        let document = web_sys::window()
+            .expect("No window")
+            .document()
+            .expect("No document");
+
+        let canvas = document
+            .get_element_by_id("the_canvas_id")
+            .expect("Failed to find the_canvas_id")
+            .dyn_into::<web_sys::HtmlCanvasElement>()
+            .expect("the_canvas_id was not a HtmlCanvasElement");
+
+        let start_result = eframe::WebRunner::new()
+            .start(
+                canvas,
+                web_options,
+                Box::new(|cc| Ok(Box::new(eframe_template::TemplateApp::new(cc)))),
+            )
+            .await;
+
+        // Remove the loading text and spinner:
+        if let Some(loading_text) = document.get_element_by_id("loading_text") {
+            match start_result {
+                Ok(_) => {
+                    loading_text.remove();
+                }
+                Err(e) => {
+                    loading_text.set_inner_html(
+                        "<p> The app has crashed. See the developer console for details. </p>",
+                    );
+                    panic!("Failed to start eframe: {e:?}");
+                }
+            }
+        }
+    });
 }
